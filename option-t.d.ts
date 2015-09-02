84b5642a67ea2030667ee54d456936d8c5cb0566
Tetsuharu OHZEKI <saneyuki.snyk@gmail.com>
2015-09-03 06:05:57 +0900

    Add API documentations to TypeScript's type definition.

--- option-t.d.ts
+++ option-t.d.ts
@@ -1,4 +1,4 @@
-/*
+/**
  * @license MIT License
  *
  * Copyright (c) 2015 Tetsuharu OHZEKI <saneyuki.snyk@gmail.com>
@@ -24,22 +24,145 @@
 
 declare module 'option-t' {
 
+    /**
+     *  The Option/Maybe type interface whose APIs are inspired
+     *  by Rust's `std::option::Option<T>`.
+     */
     interface Option<T> {
+
+        /**
+         *  Return whether the self is `Some<T>` or not.
+         */
         isSome: boolean;
+
+        /**
+         *  Return whether the self is `None` or not.
+         */
         isNone: boolean;
+
+        /**
+         *  Return the inner `T` of a `Some<T>`.
+         *
+         *  @throws {Error}
+         *    Throws if the self is a `None`.
+         */
         unwrap(): T;
+
+        /**
+         *  Return the contained value or a default value `def`.
+         *
+         *  @param  def
+         *    The default value which is used if the self is a `None`.
+         */
         unwrapOr(def: T): T;
+
+        /**
+         *  Return the contained value or compute it from a closure `fn`.
+         *
+         *  @param fn
+         *    The default value which is used if the self is a `None`.
+         */
         unwrapOrElse(fn: () => T): T;
+
+        /**
+         *  Return the inner `T` of a `Some<T>`.
+         *
+         *  @param  msg
+         *    The error message which is used if the self is a `None`.
+         *  @throws {Error}
+         *    Throws a custom error with provided `msg`
+         *    if the self value equals `None`.
+         */
         expect(msg: string): T;
+
+        /**
+         *  Map an `Option<T>` to `Option<U>` by applying a function to the contained value.
+         *
+         *  @param  fn
+         *    The function which is applied to the contained value and return the result
+         *    if the self is a `Some<T>`.
+         */
         map<U>(fn: (v: T) => U): Option<U>;
+
+        /**
+         *  Return `None` if the self is `None`,
+         *  otherwise call `fn` with the wrapped value and return the result.
+         *
+         *  @param  fn
+         *    The function which is applied to the contained value and return the result
+         *    if the self is a `Some<T>`. This result will be flattened once.
+         */
         flatMap<U>(fn: (v: T) => Option<U>): Option<U>;
+
+        /**
+         *  Apply a function `fn` to the contained value or return a default `def`.
+         *
+         *  @param  def
+         *    The default value which is used if the self is a `None`.
+         *  @param  fn
+         *    The function which is applied to the contained value and return the result
+         *    if the self is a `Some<T>`.
+         */
         mapOr<U>(def: U, fn: (v: T) => U): U;
+
+        /**
+         *  Apply a function `fn` to the contained value or produce a default result by `defFn`.
+         *
+         *  @param  defFn
+         *    The function which produces a default value which is used if the self is a `None`.
+         *  @param  fn
+         *    The function which is applied to the contained value and return the result
+         *    if the self is a `Some<T>`.
+         */
         mapOrElse<U>(def: () => U, fn: (v: T) => U): U;
+
+        /**
+         *  Return the passed value if the self is `Some<T>`,
+         *  otherwise return `None`.
+         *
+         *  @param  optb
+         *    The value which is returned if the self is a `Some<T>`.
+         */
         and<U>(optb: Option<U>): Option<U>;
+
+        /**
+         *  The alias of `Option<T>.flatMap()`.
+         *
+         *  @param  fn
+         *    The function which produces a default value which is used if the self is a `None`.
+         */
         andThen<U>(fn: (v: T) => Option<U>): Option<U>;
+
+        /**
+         *  Return the self if it contains a value, otherwise return `optb`.
+         *
+         *  @param  optb
+         *    The default value which is used if the self is a `None`.
+         */
         or(optb: Option<T>): Option<T>;
+
+        /**
+         *  Return the self if it contains a value,
+         *  otherwise call `fn` and returns the result.
+         *
+         *  @param  fn
+         *    The function which produces a default value which is used if the self is a `None`.
+         */
         orElse(fn: () => Option<T>): Option<T>;
+
+        /**
+         *  Cast the self as Promise.
+         *
+         *  If `T` is `Promise<U>` (the self is `Option<Promise<U>>`),
+         *  then the wrapped `Promise<U>` is flatten automatically by design.
+         *  So this will returns `Promise<U>`.
+         */
         asPromise(): Promise<T>;
+
+        /**
+         *  Finalize the self.
+         *  After this is called, the object's behavior is not defined.
+         */
         drop(): void;
     }
 
