6f58ebc87467cf2ebc572507f6d0a0b3cdb50049
Tetsuharu OHZEKI <saneyuki.snyk@gmail.com>
2015-12-15 06:39:55 +0900

    Enable many lint rules
    Inspired by https://github.com/karen-irc/karen/blob/master/.eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,7 @@
 // ESLint Configuration Files enables to include comments.
 // http://eslint.org/docs/configuring/#comments-in-configuration-files
 module.exports = {
+    // Derive recommended rules to detect bad smells even if eslint added a new recommended one but we forgot to add them to ours.
     'extends': 'eslint:recommended',
 
     'ecmaFeatures': {
@@ -14,11 +15,13 @@ module.exports = {
         'objectLiteralComputedProperties': true,
         'objectLiteralDuplicateProperties': true,
         'objectLiteralShorthandMethods': true,
-        'objectLiteralShorthandProperties': true
+        'objectLiteralShorthandProperties': true,
+        'spread': true,
+        'templateStrings': true,
     },
 
     'env': {
-        'node': true,
+        'node': true
     },
 
     'root': true,
@@ -26,32 +29,68 @@ module.exports = {
     'rules': {
         // Possible Errors
         'comma-dangle': [0, 'never'],
-        'no-constant-condition': 1,
+        'no-cond-assign': 2,
         'no-console': 0,
+        'no-constant-condition': 1,
+        'no-control-regex': 2,
         'no-debugger': 1,
+        'no-dupe-args': 2,
+        'no-dupe-keys': 2,
+        'no-duplicate-case': 2,
+        'no-empty-character-class': 2,
+        'no-empty': 2,
+        'no-ex-assign': 2,
         'no-extra-boolean-cast': 0,
         'no-extra-parens': 0,
+        'no-extra-semi': 1,
+        'no-func-assign': 2,
+        'no-inner-declarations': 2,
+        'no-invalid-regexp': 2,
+        'no-irregular-whitespace': 2,
+        'no-negated-in-lhs': 2,
+        'no-obj-calls': 2,
+        'no-regex-spaces': 2,
+        'no-sparse-arrays': 2,
+        'no-unexpected-multiline': 1,
+        'no-unreachable': 1,
+        'use-isnan': 2,
         'valid-jsdoc': [2, {
             'requireReturn': true,
             'requireParamDescription': false,
             'requireReturnDescription': false
         }],
-        'no-unexpected-multiline': 1,
+        'valid-typeof': 2,
 
         // Best Practices
         'block-scoped-var': 1,
+        'consistent-return': 2,
         'curly': 2,
+        'eqeqeq': 2,
         'no-alert': 1,
-        'no-caller': 1,
+        'no-caller': 2,
+        'no-case-declarations': 2,
         'no-div-regex': 2,
+        'no-empty-pattern': 2,
         'no-eq-null': 2,
+        'no-eval': 2,
+        'no-fallthrough': 2,
+        'no-implied-eval': 2,
         'no-invalid-this': 1,
+        'no-new-func': 1,
+        'no-new-wrappers': 2,
         'no-param-reassign': [1, {
             'props': true
         }],
+        'no-octal': 2,
+        'no-proto': 2,
+        'no-redeclare': 2,
+        'no-return-assign': 2,
+        'no-script-url': 2,
         'no-throw-literal': 2,
         'no-unused-expressions': 2,
         'no-useless-call': 1,
+        'no-useless-concat': 1,
+        'no-with': 2,
         'radix': 2,
 
         // Strict Mode
@@ -59,30 +98,63 @@ module.exports = {
 
         // Variables
         'init-declarations': [2, 'always'],
+        'no-catch-shadow': 2,
+        'no-delete-var': 2,
         'no-shadow': 0,
-        'no-unused-vars': 0,
+        'no-undef': 2,
+        'no-undef-init': 2,
+        'no-undefined': 0,
+        'no-unused-vars': [1, {
+            'vars': 'all',
+            'args': 'after-used',
+        }],
         'no-use-before-define': 0,
 
         // Node.js
-        'no-process-exit': 0,
         'global-require': 2,
+        'no-mixed-requires': [2, {
+            'grouping': true,
+        }],
+        'no-new-require': 2,
+        'no-path-concat': 2,
+        'no-process-exit': 0,
+        'no-sync': 2,
 
         // Stylistic Issues
-        'indent': [2, 4, {
-            'SwitchCase': 1,
+        'camelcase': [2, {
+            'properties': 'always'
         }],
         'comma-spacing': [2, {
             'before': false,
             'after': true
         }],
         'comma-style': [2, 'last'],
+        'computed-property-spacing': [2, 'never'],
+        'indent': [2, 4, {
+            'SwitchCase': 1
+        }],
+        'jsx-quotes': [1, 'prefer-single'],
+        'key-spacing': 0,
         'linebreak-style': [2, 'unix'],
+        'new-cap': 1,
+        'new-parens': 2,
+        'no-array-constructor': 2, // In almost case, we don't have to use `new Array()` without any comments.
         'no-mixed-spaces-and-tabs': 2,
-        'no-unneeded-ternary': 2,
+        'no-new-object': 2, // In almost case, we don't have to use `new Object()` without any comments.
+        'no-spaced-func': 2,
+        'no-trailing-spaces': 2,
         'no-underscore-dangle': 0,
+        'no-unneeded-ternary': 2,
         'operator-linebreak': [2, 'after'],
         'quotes': [2, 'single', 'avoid-escape'],
         'semi': [2, 'always'],
+        'semi-spacing':[2, {
+            'before': false,
+            'after': true
+        }],
+        'space-after-keywords': 1,
+        'space-infix-ops': 1,
+        'space-return-throw-case': 1,
         'space-unary-ops': [2, {
             'words': true,
             'nonwords': false
@@ -90,6 +162,7 @@ module.exports = {
         'spaced-comment': 0,
 
         // ECMAScript 6
+        'arrow-body-style': [1, 'as-needed'],
         'arrow-parens': 1,
         'arrow-spacing': [1, {
             'before': true,
@@ -100,6 +173,7 @@ module.exports = {
             'before': false,
             'after': true
         }],
+        'no-arrow-condition': 2,
         'no-class-assign': 2,
         'no-const-assign': 2,
         'no-dupe-class-members': 2,
