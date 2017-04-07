4a59674168b1181847ea151f595b35b356681ef5
Tetsuharu OHZEKI <saneyuki.snyk@gmail.com>
2017-04-07 13:39:39 +0900

    Drop flatMapForNullable
    it's too hard to undarstand that "flatMap" operation for `T | undefined`
    or `T | null`.

--- src/Undefinable/andThen.ts
+++ src/Undefinable/andThen.ts
@@ -1,7 +1,20 @@
-import { flatMapForUndefinable } from './flatMap';
-import { FlatMapFn } from './Function';
-import { Undefinable } from './Undefinable';
+import { Undefinable, isNotUndefined } from './Undefinable';
 
-export function andThenForUndefinable<T, U>(src: Undefinable<T>, fn: FlatMapFn<T, U>): Undefinable<U> {
-    return flatMapForUndefinable<T, U>(src, fn);
+/**
+ *  Returns `undefined` if the `src` is `undefined`,
+ *  otherwise calls `fn` with the value and returns the result.
+ * 
+ *  XXX:
+ *  Some languages call this operation flatmap.
+ *  But we don't provide `flatMap()` as alias of this function.
+ *  because it's too hard to undarstand that "flatMap" operation for `T | undefined`
+ */
+export function andThenForUndefinable<T, U>(src: Undefinable<T>, fn: (this: void, v: T) => Undefinable<U>): Undefinable<U> {
+    if (isNotUndefined(src)) {
+        const r = fn(src);
+        return r;
+    }
+    else {
+        return src;
+    }
 }
