b12d63c6ea7662f755d6b84309aa519c8a665d46
Tetsuharu OHZEKI <saneyuki.snyk@gmail.com>
2017-04-07 14:16:02 +0900

    Add the comment why we don't ship or/and for Nullable/Undefinable by default.

--- src/Nullable/index.ts
+++ src/Nullable/index.ts
@@ -1,4 +1,5 @@
 export * from './Nullable';
+// XXX: `and()` operation is equivalent of `a && b` so we don't ship it by default set.
 //export { andNullable as and } from './and';
 export { andThenForNullable as andThen } from './andThen';
 export { doOnNullable as do } from './do';
@@ -6,6 +7,7 @@ export { expectNotNull as expect } from './expect';
 export { mapForNullable as map } from './map';
 export { mapOrForNullable as mapOr } from './mapOr';
 export { mapOrElseForNullable as mapOrElse } from './mapOrElse';
+// XXX: `or()` operation is equivalent of `a || b` so we don't ship it by default set.
 // export { orNullable as or } from './or';
 export { orElseForNullable as orElse } from './orElse';
 export { unwrapNullable as unwrap } from './unwrap';
--- src/Undefinable/index.ts
+++ src/Undefinable/index.ts
@@ -1,4 +1,5 @@
 export * from './Undefinable';
+// XXX: `and()` operation is equivalent of `a && b` so we don't ship it by default set.
 //export { andUndefinable as and} from './and';
 export { andThenForUndefinable as andThen} from './andThen';
 export { doOnUndefinable as do } from './do';
@@ -6,7 +7,8 @@ export { expectNotUndefined as expect } from './expect';
 export { mapForUndefinable as map } from './map';
 export { mapOrForUndefinable as mapOr } from './mapOr';
 export { mapOrElseForUndefinable as mapOrElse } from './mapOrElse';
-//export { orUndefinable as or} from './or';
+// XXX: `or()` operation is equivalent of `a || b` so we don't ship it by default set.
+// export { orUndefinable as or} from './or';
 export { orElseForUndefinable as orElse} from './orElse';
 export { unwrapUndefinable as unwrap } from './unwrap';
 export { unwrapOrFromUndefinable as unwrapOr } from './unwrapOr';
