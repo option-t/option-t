f32d09c20a68b69231b1596866f89bf07a6b0b73
Tetsuharu OHZEKI <saneyuki.snyk@gmail.com>
2017-07-04 09:08:00 +0900

    unwrapMaybe() should throw more detailed message.

--- src/Maybe/unwrap.ts
+++ src/Maybe/unwrap.ts
@@ -2,5 +2,5 @@ import { expectNotNullAndUndefined } from './expect';
 import { Maybe } from './Maybe';
 
 export function unwrapMaybe<T>(v: Maybe<T>): T | never {
-    return expectNotNullAndUndefined(v, 'called with `null`');
+    return expectNotNullAndUndefined(v, 'called with `null` or `undefined`');
 }
