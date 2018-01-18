12d36b3a7b5542ab6e00720461bc4bdf1055f183
Tetsuharu OHZEKI <saneyuki.snyk@gmail.com>
2018-01-18 16:28:48 +0900

    Add tap for Nullable

--- src/Nullable/do.ts
+++ src/Nullable/do.ts
@@ -1,10 +1,3 @@
-import { TapFn } from '../utils/Function';
-import { Nullable } from './Nullable';
-
-export function doOnNullable<T>(v: Nullable<T>, fn: TapFn<T>): void {
-    if (v === null) {
-        return;
-    }
-
-    fn(v);
-}
+export {
+    tapNullable as doOnMaybe
+} from './tap';
