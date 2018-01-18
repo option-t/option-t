16862d8aa73ee8634339ffb81b9876faf53b8e77
Tetsuharu OHZEKI <saneyuki.snyk@gmail.com>
2018-01-18 16:35:47 +0900

    Add tap for PlainResult

--- src/PlainResult/do.ts
+++ src/PlainResult/do.ts
@@ -1,21 +1,5 @@
-import { TapFn } from '../utils/Function';
-import { Result } from './Result';
-
-function noop<T>(_v: T) {}
-
-export function doOnOk<T, E>(v: Result<T, E>, fn: TapFn<T>): void {
-    return doOnBoth(v, fn, noop);
-}
-
-export function doOnErr<T, E>(v: Result<T, E>, fn: TapFn<E>): void {
-    return doOnBoth(v, noop, fn);
-}
-
-export function doOnBoth<T, E>(src: Result<T, E>, okFn: TapFn<T>, errFn: TapFn<E>): void {
-    if (src.ok) {
-        okFn(src.val);
-    }
-    else {
-        errFn(src.err);
-    }
-}
+export {
+    tapOk as doOnOk,
+    tapErr as doOnErr,
+    tapBoth as doOnBoth,
+} from './tap';
---
+++ src/PlainResult/tap.ts
@@ -0,0 +1,21 @@
+import { TapFn } from '../utils/Function';
+import { Result } from './Result';
+
+function noop<T>(_v: T) {}
+
+export function tapOk<T, E>(v: Result<T, E>, fn: TapFn<T>): void {
+    return tapBoth(v, fn, noop);
+}
+
+export function tapErr<T, E>(v: Result<T, E>, fn: TapFn<E>): void {
+    return tapBoth(v, noop, fn);
+}
+
+export function tapBoth<T, E>(src: Result<T, E>, okFn: TapFn<T>, errFn: TapFn<E>): void {
+    if (src.ok) {
+        okFn(src.val);
+    }
+    else {
+        errFn(src.err);
+    }
+}
