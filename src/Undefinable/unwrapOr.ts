4bccf628afcbd21853ffed384c4411ec4d89852c
Tetsuharu OHZEKI <saneyuki.snyk@gmail.com>
2018-02-01 05:03:50 +0900

    Stabilize unwrapOr for Maybe/Nullable/Undefinable

--- src/Undefinable/unwrapOr.ts
+++ src/Undefinable/unwrapOr.ts
@@ -1,5 +1,14 @@
 import { Undefinable } from './Undefinable';
 
+/**
+ *  Return _v_ as `T` if the passed _v_ is not `undefined`.
+ *  Otherwise, return _def_.
+ *
+ *  * _def_ must not be `Undefinable<*>`.
+ *  * We may introduce an assert to check _def_ is not `Undefinable<*>`
+ *    as the running time checking for the future.
+ *      * See https://github.com/karen-irc/option-t/issues/254.
+ */
 export function unwrapOrFromUndefinable<T>(v: Undefinable<T>, def: T): T {
     return (v !== undefined) ? v : def;
 }
---
+++ test/Undefinable/test_unwrap_or.js
@@ -0,0 +1,74 @@
+'use strict';
+
+const assert = require('assert');
+const { unwrapOrFromUndefinable } = require('../../cjs/Undefinable/unwrapOr');
+const { nonNullableValue } = require('../utils');
+
+describe('Undefinable::unwrapOr', () => {
+    describe('pass the value', () => {
+        for (const value of nonNullableValue) {
+            describe(String(value), () => {
+                const DEFAULT_VAL = Math.random();
+                const EXPECTED = value;
+                // eslint-disable-next-line
+                let result;
+                let e = null;
+                before(() => {
+                    try {
+                        result = unwrapOrFromUndefinable(EXPECTED, DEFAULT_VAL);
+                    }
+                    catch (expected) {
+                        e = expected;
+                    }
+                });
+
+                it('should the expected result', () => {
+                    assert.strictEqual(result, EXPECTED);
+                });
+
+                it('should not throw error', () => {
+                    assert.strictEqual(e, null);
+                });
+            });
+        }
+    });
+
+    describe('pass null', () => {
+        const DEFAULT_VAL = Math.random();
+        // eslint-disable-next-line
+        let result;
+        before(() => {
+            result = unwrapOrFromUndefinable(null, DEFAULT_VAL);
+        });
+
+        it('should be the default', () => {
+            assert.strictEqual(result, null);
+        });
+    });
+
+    describe('pass undefined', () => {
+        const DEFAULT_VAL = Math.random();
+        // eslint-disable-next-line
+        let result;
+        before(() => {
+            result = unwrapOrFromUndefinable(undefined, DEFAULT_VAL);
+        });
+
+        it('should be the default', () => {
+            assert.strictEqual(result, DEFAULT_VAL);
+        });
+    });
+
+    describe.skip('should not accept Maybe<*> as default (TODO: #254)', () => {
+        const testcases = [
+            [undefined, undefined],
+        ];
+        for (const [src, def] of testcases) {
+            it(`v = ${String(src)}, def = ${String(def)}`, () => {
+                assert.throws(() => {
+                    unwrapOrFromUndefinable(src, def);
+                }, TypeError);
+            });
+        }
+    });
+});
