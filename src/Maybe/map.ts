7cdf94d3211713847388df01a3e66c7827c826ad
Tetsuharu OHZEKI <saneyuki.snyk@gmail.com>
2018-01-31 15:10:47 +0900

    Add the comment why we check the returned value type from selector in map operation
    If `U` is `Maybe<SomeType>`, we think naturally the returned value of this function would be
    the nested type `Maybe<Maybe<SomeType>>`. But this type means `(SomeType | null | undefined) | null | undefined`.
    So a type checker would recognize this type as `SomeType | null | undefined`. So it's flattened.
    Then the user should call `andThen` (_flatmap_) operation instead of this.
    
    And we should throw the error for it.

--- src/Maybe/map.ts
+++ src/Maybe/map.ts
@@ -5,7 +5,12 @@ import { Maybe } from './Maybe';
 
 export function mapForMaybe<T, U>(src: Maybe<T>, selector: MapFn<T, U>): Maybe<U> {
     if (src !== undefined && src !== null) {
-        const r = selector(src);
+        const r: U = selector(src);
+        // XXX:
+        // If `U` is `Maybe<SomeType>`, we think naturally the returned value of this function would be
+        // the nested type `Maybe<Maybe<SomeType>>`. But this type means `(SomeType | null | undefined) | null | undefined`.
+        // So a type checker would recognize this type as `SomeType | null | undefined`. So it's flattened.
+        // Then the user should call `andThen` (_flatmap_) operation instead of this.
         return expectNotNullAndUndefined(r, ERR_MSG_SELECTOR);
     }
     else {
