8ba114d336a0c517afaf8672bf8ef2c764df24ce
Tetsuharu OHZEKI <saneyuki.snyk@gmail.com>
2018-02-03 04:33:01 +0900

    Return the input value on tap() for Maybe/Nullable/Undefinable

--- src/Nullable/tap.ts
+++ src/Nullable/tap.ts
@@ -2,12 +2,14 @@ import { TapFn } from '../shared/Function';
 import { Nullable } from './Nullable';
 
 /**
- *  Call _fn_ with _v_ if _v_ is not `null`.
+ *  * Return _v_ directly.
+ *      * This value is passed as the input. But it maybe mutated by calling _fn_.
+ *  * Call _fn_ with _v_ if _v_ is not `null`.
  */
-export function tapNullable<T>(v: Nullable<T>, fn: TapFn<T>): void {
-    if (v === null) {
-        return;
+export function tapNullable<T>(v: Nullable<T>, fn: TapFn<T>): Nullable<T> {
+    if (v !== null) {
+        fn(v);
     }
 
-    fn(v);
+    return v;
 }
--- test/Maybe/test_tap.js
+++ test/Maybe/test_tap.js
@@ -7,12 +7,14 @@ const { nonNullableValue } = require('../utils');
 
 describe('Maybe::tap', () => {
     describe('pass the value', () => {
-        for (const value of nonNullableValue) {
-            describe(String(value), () => {
+        for (const input of nonNullableValue) {
+            describe(String(input), () => {
                 let called = 0;
+                // eslint-disable-next-line
+                let result;
 
                 before(() => {
-                    tapMaybe(value, (_v) => {
+                    result = tapMaybe(input, (_v) => {
                         called += 1;
                     });
                 });
@@ -20,33 +22,50 @@ describe('Maybe::tap', () => {
                 it('should call selector fn', () => {
                     assert.strictEqual(called, 1);
                 });
+
+                it('should be input', () => {
+                    assert.strictEqual(result, input);
+                });
             });
         }
     });
 
     describe('pass null', () => {
+        // eslint-disable-next-line
+        let result;
         let called = 0;
+
         before(() => {
-            tapMaybe(null, (_v) => {
+            result = tapMaybe(null, (_v) => {
                 called += 1;
             });
         });
 
         it('should not call selector fn', () => {
             assert.strictEqual(called, 0);
+        });
+
+        it('should be input', () => {
+            assert.strictEqual(result, null);
         });
     });
 
     describe('pass undefined', () => {
+        // eslint-disable-next-line
+        let result;
         let called = 0;
         before(() => {
-            tapMaybe(undefined, (_v) => {
+            result = tapMaybe(undefined, (_v) => {
                 called += 1;
             });
         });
 
         it('should not call selector fn', () => {
             assert.strictEqual(called, 0);
+        });
+
+        it('should be input', () => {
+            assert.strictEqual(result, undefined);
         });
     });
 });
