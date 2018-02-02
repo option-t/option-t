95d51df0839552403b5e120d6df8add68d22d27f
Tetsuharu OHZEKI <saneyuki.snyk@gmail.com>
2018-02-02 20:33:48 +0900

    Stabilize or() for Maybe/Nullable/Undefinable

--- src/Maybe/or.ts
+++ src/Maybe/or.ts
@@ -1,5 +1,9 @@
 import { Maybe } from './Maybe';
 
+/**
+ *  Return _a_ if _a_ is not `null` and `undefined`.
+ *  Otherwise, return _b_.
+ */
 export function orForMaybe<T>(a: Maybe<T>, b: Maybe<T>): Maybe<T> {
     return (a !== undefined && a !== null) ? a : b;
 }
--- src/Undefinable/or.ts
+++ src/Undefinable/or.ts
@@ -1,5 +1,9 @@
 import { Undefinable } from './Undefinable';
 
+/**
+ *  Return _a_ if _a_ is not `undefined`.
+ *  Otherwise, return _b_.
+ */
 export function orForUndefinable<T>(a: Undefinable<T>, b: Undefinable<T>): Undefinable<T> {
     return (a !== undefined) ? a : b;
 }
