60121d3e8fd76c2890ad8d2900b10acc485c6beb
Tetsuharu OHZEKI <saneyuki.snyk@gmail.com>
2018-02-05 11:39:40 +0900

    Implement okOrElse() for PlainOption

---
+++ src/PlainOption/okOrElse.ts
@@ -0,0 +1,18 @@
+import { Option } from './Option';
+import { Result, createErr, createOk } from '../PlainResult/Result';
+import { RecoveryFn } from '../utils/Function';
+
+/**
+ * Transforms the `Option<T>` into a `Result<T, E>`, mapping `Some(v)` to `Ok(v)` and `None` to `Err(err())`.
+ */
+export function okOrElseForPlainOption<T, E>(input: Option<T>, err: RecoveryFn<E>): Result<T, E> {
+    if (input.ok) {
+        const v = createOk<T>(input.val);
+        return v;
+    }
+    else {
+        const e: E = err();
+        const v = createErr<E>(e);
+        return v;
+    }
+}
