a2ce2840a9d69e0ab4a8a5c7c5c457969fb2510f
Tetsuharu OHZEKI <saneyuki.snyk@gmail.com>
2018-07-04 00:39:31 +0900

    Port all tests for PlainResult to avajs

---
+++ ava_test/PlainResult/tap/test_tap_both.js
@@ -0,0 +1,39 @@
+import test from 'ava';
+
+const {
+    createErr,
+    createOk,
+} = require('../../../cjs/PlainResult/Result');
+const {
+    tapBoth,
+} = require('../../../cjs/PlainResult/tap');
+
+test('input is Ok()', (t) => {
+    t.plan(3);
+    const INPUT_INNER = Symbol('input');
+
+    const input = createOk(INPUT_INNER);
+    const actual = tapBoth(input, (v) => {
+        t.pass('should call the tap ok fn');
+        t.is(v, INPUT_INNER, 'should be the expected arg');
+    }, (_e) => {
+        t.fail('should not call this path');
+    });
+
+    t.is(input, actual, 'should be the expect returned');
+});
+
+test('input is Err()', (t) => {
+    t.plan(3);
+    const INPUT_INNER = Symbol('input');
+
+    const input = createErr(INPUT_INNER);
+    const actual = tapBoth(input, (_v) => {
+        t.fail('should not call this path');
+    }, (e) => {
+        t.pass('should call the tap err fn');
+        t.is(e, INPUT_INNER, 'should be the expected arg');
+    });
+
+    t.is(input, actual, 'should be the expect returned');
+});
---
+++ ava_test/PlainResult/tap/test_tap_ok.js
@@ -0,0 +1,34 @@
+import test from 'ava';
+
+const {
+    createErr,
+    createOk,
+} = require('../../../cjs/PlainResult/Result');
+const {
+    tapOk,
+} = require('../../../cjs/PlainResult/tap');
+
+test('input is Ok()', (t) => {
+    t.plan(3);
+    const INPUT_INNER = Symbol('input');
+
+    const input = createOk(INPUT_INNER);
+    const actual = tapOk(input, (v) => {
+        t.pass('should call the tap fn');
+        t.is(v, INPUT_INNER, 'should be the expected arg');
+    });
+
+    t.is(input, actual, 'should be the expect returned');
+});
+
+test('input is Err()', (t) => {
+    t.plan(1);
+    const INPUT_INNER = Symbol('input');
+
+    const input = createErr(INPUT_INNER);
+    const actual = tapOk(input, (_v) => {
+        t.pass('should not call the tap fn');
+    });
+
+    t.is(input, actual, 'should be the expect returned');
+});
