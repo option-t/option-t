1021370bb4b833558f70ffa04bdbe969015ed4c4
Tetsuharu OHZEKI <tetsuharu.ohzeki@gmail.com>
2019-03-04 01:44:20 +0900

    Implement transpose() for PlainResult<Option<T>, E>

---
+++ src/PlainResult/transpose.ts
@@ -0,0 +1,29 @@
+import { Option, createSome, createNone, isNone, Some } from '../PlainOption/Option';
+import { Result, Ok, Err, isErr, createOk, createErr } from './Result';
+
+/**
+ *  Transposes a `Result` of an `Option` into an `Option` of a `Result`.
+ *
+ *  - `Ok(Some(v))` -> `Some(Ok(v))`
+ *  - `Ok(None)` -> `None`
+ *  - `Err(e)` -> `Some(Err(e))`
+ */
+export function transposeForResult<T, E>(self: Result<Option<T>, E>): Option<Result<T, E>> {
+    if (isErr(self)) {
+        const e: E = self.err;
+        const newErr: Err<E> = createErr(e);
+        const r: Some<Err<E>> = createSome<Err<E>>(newErr);
+        return r;
+    }
+
+    const inner: Option<T> = self.val;
+    if (isNone(inner)) {
+        const r = createNone();
+        return r;
+    }
+
+    const v: T = inner.val;
+    const innerV: Ok<T> = createOk(v);
+    const r: Option<Ok<T>> = createSome(innerV);
+    return r;
+}
