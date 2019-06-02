d1892692028caf8030c508d145e46614b3a4761a
Tetsuharu OHZEKI <tetsuharu.ohzeki@gmail.com>
2019-06-02 23:52:16 +0900

    Add comments for `map` operator for PlainOption/PlainResult

--- src/PlainOption/map.ts
+++ src/PlainOption/map.ts
@@ -1,6 +1,12 @@
 import { MapFn } from '../shared/Function';
 import { Option, createSome } from './Option';
 
+/**
+ *  Maps a `Option<T>` to `Option<U>` by applying a _selector_ function
+ *  to an contained `Some(T)` value, leaving an `None` value untouched.
+ *
+ *  This function can be used to compose the results of two functions.
+ */
 export function mapForOption<T, U>(src: Option<T>, selector: MapFn<T, U>): Option<U> {
     if (src.ok) {
         const r: U = selector(src.val);
--- src/PlainResult/map.ts
+++ src/PlainResult/map.ts
@@ -1,6 +1,12 @@
 import { MapFn } from '../shared/Function';
 import { Result, Err, createOk } from './Result';
 
+/**
+ *  Maps a `Result<T, E>` to `Result<U, E>` by applying a _selector_ function
+ *  to an contained `Ok(T)` value, leaving an `Err(E)` value untouched.
+ *
+ *  This function can be used to compose the results of two functions.
+ */
 export function mapForResult<T, U, E>(src: Result<T, E>, selector: MapFn<T, U>): Result<U, E> {
     if (src.ok) {
         const r: U = selector(src.val);
