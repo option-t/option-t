b661d3dd764f31e5b7c81e4fa38a4d65e69685af
Tetsuharu OHZEKI <tetsuharu.ohzeki@gmail.com>
2019-06-02 23:37:02 +0900

    Add comments for `unwrap` operator for PlainOption/PlainResult

--- src/PlainOption/unwrap.ts
+++ src/PlainOption/unwrap.ts
@@ -1,6 +1,12 @@
 import { expectIsSome } from './expect';
 import { Option } from './Option';
 
+/**
+ *  Return the inner `T` of a `Some(T)`.
+ *
+ *  @throws {TypeError}
+ *      Throws if the self is a `None`.
+ */
 export function unwrapOption<T>(v: Option<T>): T | never {
     return expectIsSome(v, 'called with `None`');
 }
--- src/PlainResult/unwrap.ts
+++ src/PlainResult/unwrap.ts
@@ -1,10 +1,22 @@
 import { expectIsOk, expectIsErr } from './expect';
 import { Result } from './Result';
 
+/**
+ *  Return the inner `T` of a `Ok(T)`.
+ *
+ *  @throws {TypeError}
+ *      Throws if the self is a `Err`.
+ */
 export function unwrapFromResult<T, E>(v: Result<T, E>): T | never {
     return expectIsOk(v, 'called with `Err`');
 }
 
+/**
+ *  Return the inner `E` of a `Err(E)`.
+ *
+ *  @throws {TypeError}
+ *      Throws if the self is a `Ok`.
+ */
 export function unwrapErrFromResult<T, E>(v: Result<T, E>): E | never {
     return expectIsErr(v, 'called with `Ok`');
 }
