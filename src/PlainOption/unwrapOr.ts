3cb3572a33dee494f71f726848ac87ec6d3602de
Tetsuharu OHZEKI <tetsuharu.ohzeki@gmail.com>
2019-06-02 23:43:48 +0900

    Add comments for `unwrapOr` operator for PlainOption/PlainResult

--- src/PlainOption/unwrapOr.ts
+++ src/PlainOption/unwrapOr.ts
@@ -1,5 +1,9 @@
 import { Option } from './Option';
 
+/**
+ *  Unwraps a result _v_, returns the content of an `Some(T)`.
+ *  If the value is an `None` then return _def_.
+ */
 export function unwrapOrFromOption<T>(v: Option<T>, def: T): T {
     return v.ok ? v.val : def;
 }
--- src/PlainResult/unwrapOr.ts
+++ src/PlainResult/unwrapOr.ts
@@ -1,5 +1,9 @@
 import { Result } from './Result';
 
+/**
+ *  Unwraps a result _v_, returns the content of an `Ok(T)`.
+ *  If the value is an `Err(E)` then return _def_.
+ */
 export function unwrapOrFromResult<T, E>(v: Result<T, E>, def: T): T {
     return v.ok ? v.val : def;
 }
