286c2a195b4d34abcfd517601add35f5dd912b26
Tetsuharu OHZEKI <tetsuharu.ohzeki@gmail.com>
2019-06-02 23:40:18 +0900

    Add comments for `mapOrElse` operator for PlainOption/PlainResult

--- src/PlainOption/mapOrElse.ts
+++ src/PlainOption/mapOrElse.ts
@@ -1,6 +1,11 @@
 import { MapFn, RecoveryFn } from '../shared/Function';
 import { Option } from './Option';
 
+/**
+ *  Maps a `Option<T>` to `U` by applying _selector_ to a contained `Some(T)` value in _src_,
+ *  or a _def_ function to a contained `None` value in _src_.
+ *  This function can be used to unpack a successful result while handling an error.
+ */
 export function mapOrElseForOption<T, U>(src: Option<T>, def: RecoveryFn<U>, selector: MapFn<T, U>): U {
     let r: U;
     if (src.ok) {
--- src/PlainResult/mapOrElse.ts
+++ src/PlainResult/mapOrElse.ts
@@ -1,6 +1,11 @@
 import { MapFn, RecoveryWithErrorFn } from '../shared/Function';
 import { Result } from './Result';
 
+/**
+ *  Maps a `Result<T, E>` to `U` by applying _selector_ to a contained `Ok(T)` value in _src_,
+ *  or a _fallback_ function to a contained `Err(E)` value in _src_.
+ *  This function can be used to unpack a successful result while handling an error.
+ */
 export function mapOrElseForResult<T, E, U>(src: Result<T, E>, fallback: RecoveryWithErrorFn<E, U>, selector: MapFn<T, U>): U {
     if (src.ok) {
         const r: U = selector(src.val);
