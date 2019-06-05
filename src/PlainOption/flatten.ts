5e9a62a0abbaabdb399a6d4eb915c182724e3d48
Tetsuharu OHZEKI <tetsuharu.ohzeki@gmail.com>
2019-06-05 23:33:42 +0900

    Implement flatten() for PlainOption

---
+++ src/PlainOption/flatten.ts
@@ -0,0 +1,14 @@
+import { Option } from './Option';
+import { andThenForOption } from './andThen';
+
+function flatten<T>(input: Option<T>): Option<T> {
+    return input;
+}
+
+/**
+ *  Flatten the inner value of _input_.
+ */
+export function flattenForOption<T>(input: Option<Option<T>>): Option<T> {
+    const r: Option<T> = andThenForOption(input, flatten);
+    return r;
+}
