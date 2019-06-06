ffbb454553589a391f6e61e426b412e300944e63
Tetsuharu OHZEKI <tetsuharu.ohzeki@gmail.com>
2019-06-07 01:41:54 +0900

    Add the test to ensure properties on objects
    We add these properties on creating by factory functions
    to stabilize their object Shape (SpiderMonkey)/Structure (JavaScriptCore)/Hidden Class (V8).
    
    * `PlainOption::None.val`
    * `PlainResult::Ok.err`
    * `PlainResult::Err.val`
    
    This change adds the test case to ensure to do it.

--- ava_test/PlainResult/test_object_shape.js
+++ ava_test/PlainResult/test_object_shape.js
@@ -10,6 +10,8 @@ test('The shape of PlainResult::Ok', (t) => {
     t.true(actual.ok, 'Ok.ok');
     t.is(actual.val, INNER_VAL, 'Ok.val');
     t.is(actual.err, undefined, 'Ok.err');
+    // eslint-disable-next-line no-prototype-builtins
+    t.true(actual.hasOwnProperty('err'), '`err` should be added on creating this object to stabilize object\'s Shape/Structure/Hidden Class');
 });
 
 test('The shape of PlainResult::Err', (t) => {
@@ -19,5 +21,7 @@ test('The shape of PlainResult::Err', (t) => {
 
     t.false(actual.ok, 'Err.ok');
     t.is(actual.val, undefined, 'Err.val');
+    // eslint-disable-next-line no-prototype-builtins
+    t.true(actual.hasOwnProperty('val'), '`val` should be added on creating this object to stabilize object\'s Shape/Structure/Hidden Class');
     t.is(actual.err, INNER_VAL, 'Err.err');
 });
