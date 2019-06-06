253b620d521fc7d59aa1a22186ba89430c72b551
Tetsuharu OHZEKI <tetsuharu.ohzeki@gmail.com>
2019-06-07 00:14:42 +0900

    Add and() for PlainOption/PlainResult

--- ava_test/PlainOption/test_and.js
+++ ava_test/PlainOption/test_and.js
@@ -1,6 +1,53 @@
 import test from 'ava';
 
-test.todo('Some');
+const { createSome, createNone } = require('../../__dist/cjs/PlainOption/Option');
+const { andForOption } = require('../../__dist/cjs/PlainOption/and');
 
-test.todo('None');
+test('a=Some, b=Some', (t) => {
+    const EXPECTED = Symbol('expected');
+    const NOT_EXPECTED = Symbol('not expected');
+
+    const a = createSome(NOT_EXPECTED);
+    const b = createSome(EXPECTED);
+
+    const actual = andForOption(a, b);
+
+    t.is(actual, b, 'should return b');
+    t.true(actual.ok, 'should be Some');
+    t.is(actual.val, EXPECTED, 'should be the inner value');
+});
+
+test('a=Some, b=None', (t) => {
+    const NOT_EXPECTED = Symbol('not expected');
+
+    const a = createSome(NOT_EXPECTED);
+    const b = createNone();
+
+    const actual = andForOption(a, b);
+
+    t.is(actual, b, 'should return b');
+    t.false(actual.ok, 'should be None');
+});
+
+test('a=None, b=Some', (t) => {
+    const NOT_EXPECTED = Symbol('not expected');
+
+    const a = createNone();
+    const b = createSome(NOT_EXPECTED);
+
+    const actual = andForOption(a, b);
+
+    t.is(actual, a, 'should return a');
+    t.false(actual.ok, 'should be None');
+});
+
+test('a=None, b=None', (t) => {
+    const a = createNone();
+    const b = createNone();
+
+    const actual = andForOption(a, b);
+
+    t.is(actual, a, 'should return a');
+    t.false(actual.ok, 'should be None');
+});
 
