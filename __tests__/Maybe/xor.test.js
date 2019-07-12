10f9296e18a567ed6b2db3959fe160aa518ea8d3
Tetsuharu OHZEKI <tetsuharu.ohzeki@gmail.com>
2019-07-12 23:42:30 +0900

    Implement xor() for Maybe

--- __tests__/Maybe/xor.test.js
+++ __tests__/Maybe/xor.test.js
@@ -14,21 +14,21 @@ function testCase(a, b, expected) {
 }
 
 const TABLE = [
-    testCase(SOME_VAL_A, SOME_VAL_B, null),
+    testCase(SOME_VAL_A, SOME_VAL_B, undefined),
     testCase(SOME_VAL_A, null, SOME_VAL_A),
     testCase(null, SOME_VAL_B, SOME_VAL_B),
-    testCase(null, null, null),
+    testCase(null, null, undefined),
 
     testCase(SOME_VAL_A, undefined, SOME_VAL_A),
     testCase(undefined, SOME_VAL_B, SOME_VAL_B),
-    testCase(undefined, undefined, null),
+    testCase(undefined, undefined, undefined),
 
-    testCase(null, undefined, null),
-    testCase(undefined, null, null),
+    testCase(null, undefined, undefined),
+    testCase(undefined, null, undefined),
 ];
 
 for (const { a, b, expected } of TABLE) {
-    test.skip(`a=${String(a)}, b=${String(b)}`, (t) => {
+    test(`a=${String(a)}, b=${String(b)}`, (t) => {
 
         const actual = xorForMaybe(a, b);
         t.is(actual, expected, 'should be `' + String(expected) + '`');
--- src/Maybe/xor.ts
+++ src/Maybe/xor.ts
@@ -1,6 +1,25 @@
-import { Maybe } from './Maybe';
+import { Maybe, isNotNullAndUndefined } from './Maybe';
 
-export function xorForMaybe<T>(_a: Maybe<T>, _b: Maybe<T>): Maybe<T> {
-    // TODO: #410
-    throw new TypeError('unimplemented');
+/**
+ *  Return one of this pattern:
+ *
+ *  1. a=others, b=null|undefined => _a_
+ *  2. a=null|undefined, b=others => _b_
+ *  3. Others => `undefined`
+ */
+export function xorForMaybe<T>(a: Maybe<T>, b: Maybe<T>): Maybe<T> {
+    const aIsSome = isNotNullAndUndefined(a);
+    const bIsSome = isNotNullAndUndefined(b);
+
+    if (aIsSome && !bIsSome) {
+        return a;
+    }
+    else if (!aIsSome && bIsSome) {
+        return b;
+    }
+    else {
+        // XXX: We can choose both `null` and `undefined`.
+        // But we return `undefined` to sort with [Optional Chaining](https://github.com/TC39/proposal-optional-chaining)
+        return undefined;
+    }
 }
