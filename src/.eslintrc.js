261918b134826823b0623af4891e8d8f1e304839
Tetsuharu OHZEKI <tetsuharu.ohzeki@gmail.com>
2019-08-28 21:21:03 +0900

    Add ESLint specific tsconfig

--- src/.eslintrc.js
+++ src/.eslintrc.js
@@ -15,7 +15,7 @@ module.exports = {
 
     'parserOptions': {
         'sourceType': 'module',
-        'project': path.resolve(__dirname, '../tsconfig.json'),
+        'project': path.resolve(__dirname, '../tsconfig_eslint.json'),
     },
 
     'env': {
