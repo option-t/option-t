24057dea2825a3c4152720fd14fad061f1486dea
Tetsuharu OHZEKI <tetsuharu.ohzeki@gmail.com>
2019-10-26 15:56:08 +0900

    Enable typescript-eslint for __tests__/

--- __tests__/.eslintrc.js
+++ __tests__/.eslintrc.js
@@ -1,12 +1,30 @@
-/* eslint-env commonjs */
+/* eslint-env node */
 /*eslint quote-props: [2, "always"] */
 
 'use strict'; // eslint-disable-line strict
 
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const path = require('path');
+
 // ESLint Configuration Files enables to include comments.
 // http://eslint.org/docs/configuring/#comments-in-configuration-files
 module.exports = {
+    'extends': [
+        '../tools/eslint/eslintrc_typescript.js',
+    ],
+
     'parserOptions': {
         'sourceType': 'module',
+        'project': path.resolve(__dirname, '../tsconfig_eslint.json'),
     },
+
+    'env': {
+        'es6': true,
+        'node': false,
+        'commonjs': false,
+    },
+
+    'rules': {
+        '@typescript-eslint/no-magic-numbers': 'off',
+    }
 };
