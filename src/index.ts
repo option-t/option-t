4160f9abc2027bf8ae14f94a6bbebb39d227bc0a
Tetsuharu OHZEKI <tetsuharu.ohzeki@gmail.com>
2019-12-05 23:23:46 +0900

    Export all public APIs from `src/index.js`.
    * By this change, user will be able to use `import {} from 'option-t';`
      without any redundant path.
    * We don't provide class versions `Result` and `Option` by this change.
      They are deprecated APIs.
    * Previously, this kind of exporting would prevent tree shaking.
      However, with ES Module, we don't mind such problem.

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import * as Maybe from './Maybe';
+import * as Nullable from './Nullable';
+import * as Undefinable from './Undefinable';
+import * as PlainOption from './PlainOption';
+import * as PlainResult from './PlainResult';
+
+export {
+    Maybe,
+    Nullable,
+    Undefinable,
+    PlainOption,
+    PlainResult,
+};
