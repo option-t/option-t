427574841f6a69bbce304174c89b3f12efec6f37
Tetsuharu OHZEKI <tetsuharu.ohzeki@gmail.com>
2019-12-05 22:49:13 +0900

    Share the code both ImportDeclaration and ExportNamedDeclaration

--- tools/babel/babel-plugin-add-mjs-suffix.js
+++ tools/babel/babel-plugin-add-mjs-suffix.js
@@ -3,56 +3,45 @@
 const EXTENSION = '.mjs';
 
 function withExtension(name) {
-    return `${name}.mjs`;
+    return `${name}${EXTENSION}`;
+}
+
+function rewriter(t, path, _state, declarationFactory) {
+    const node = path.node;
+    const source = node.source;
+    if (t.isStringLiteral(source) && source.value.endsWith(EXTENSION)) {
+        return;
+    }
+
+    if (node.specifiers.length === 0) {
+        return;
+    }
+
+    const specifiers = node.specifiers;
+
+    const newSource = t.stringLiteral(withExtension(source.value));
+    const declaration = declarationFactory(specifiers, newSource);
+
+    path.replaceWith(declaration);
 }
 
 function babelAddMjsSuffixPlugin({ types: t }) {
     return {
         visitor: {
-            ImportDeclaration(path, _state) {
-                const node = path.node;
-                const source = node.source;
-                if (
-                    t.isStringLiteral(source) &&
-                    source.value.endsWith(EXTENSION)
-                ) {
-                    return;
-                }
-
-                if (node.specifiers.length === 0) {
-                    return;
-                }
-
-                const specifiers = node.specifiers;
-
-                const newSource = t.stringLiteral(withExtension(source.value));
-                const declaration = t.importDeclaration(specifiers, newSource);
-                t.assertImportDeclaration(declaration);
-
-                path.replaceWith(declaration);
+            ImportDeclaration(path, state) {
+                rewriter(t, path, state, (specifiers, source) => {
+                    const d = t.importDeclaration(specifiers, source);
+                    t.assertImportDeclaration(d);
+                    return d;
+                });
             },
 
-            ExportNamedDeclaration(path, _state) {
-                const node = path.node;
-                const source = node.source;
-                if (
-                    t.isStringLiteral(source) &&
-                    source.value.endsWith(EXTENSION)
-                ) {
-                    return;
-                }
-
-                if (node.specifiers.length === 0) {
-                    return;
-                }
-
-                const specifiers = node.specifiers;
-
-                const newSource = t.stringLiteral(withExtension(source.value));
-                const declaration = t.exportNamedDeclaration(null, specifiers, newSource);
-                t.assertExportNamedDeclaration(declaration);
-
-                path.replaceWith(declaration);
+            ExportNamedDeclaration(path, state) {
+                rewriter(t, path, state, (specifiers, source) => {
+                    const d = t.exportNamedDeclaration(null, specifiers, source);
+                    t.assertExportNamedDeclaration(d);
+                    return d;
+                });
             },
         },
     };
