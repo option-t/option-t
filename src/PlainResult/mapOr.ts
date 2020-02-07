2fb02151872aeb145c75952b480365acc7562181
Tetsuharu OHZEKI <tetsuharu.ohzeki@gmail.com>
2020-02-07 16:34:40 +0900

    Implement mapOr() for PlainResult
    This operation is stabilized to rust v1.41.
    We already had `mapOrElse()` for PlainResult. But this one is not so.

---
+++ src/PlainResult/mapOr.ts
@@ -0,0 +1,19 @@
+import { Result } from './Result';
+import { MapFn } from '../shared/Function';
+
+/**
+ *  Return the result of _selector_ with using _src_ as an argument for it if _src_ is `Ok(T)`.
+ *  Otherwise, return _def_.
+ *
+ *  Basically, this operation is a combination `map()` and `unwrapOr()`.
+ */
+export function mapOrForResult<T, E, U>(src: Result<T, E>, def: U, selector: MapFn<T, U>): U {
+    let r: U;
+    if (src.ok) {
+        r = selector(src.val);
+    }
+    else {
+        r = def;
+    }
+    return r;
+}
