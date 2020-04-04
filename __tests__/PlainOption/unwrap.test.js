40d5ff9b1334981a26de7cb0ad6cf4cfeb30aeba
Tetsuharu Ohzeki <tetsuharu.ohzeki@gmail.com>
2020-04-04 23:52:34 +0900

    Add test cases for PlainOption/unwrap

--- __tests__/PlainOption/unwrap.test.js
+++ __tests__/PlainOption/unwrap.test.js
@@ -1,6 +1,24 @@
 import test from 'ava';
 
-test.todo('Some');
+import {
+    createSome,
+    createNone,
+} from '../../__dist/cjs/PlainOption/Option';
+import { unwrapOption } from '../../__dist/cjs/PlainOption/unwrap';
 
-test.todo('None');
+const EXPECTED = Symbol('some_value');
 
+test('Some(T)', (t) => {
+    const input = createSome(EXPECTED);
+    t.is(unwrapOption(input), EXPECTED);
+});
+
+test('None', (t) => {
+    t.throws(() => {
+        const input = createNone();
+        unwrapOption(input);
+    }, {
+        instanceOf: TypeError,
+        message: 'called with `None`',
+    });
+});
