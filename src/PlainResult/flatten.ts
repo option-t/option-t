ae5e5a307ce82d939cd1420ce1fe166d6261fc68
Tetsuharu Ohzeki <tetsuharu.ohzeki@gmail.com>
2020-04-04 15:55:19 +0900

    Implement flatten() for PlainResult

---
+++ src/PlainResult/flatten.ts
@@ -0,0 +1,14 @@
+import { Result } from './Result';
+import { andThenForResult } from './andThen';
+
+function flatten<T, E>(input: Result<T, E>): Result<T, E> {
+    return input;
+}
+
+/**
+ * Converts from `Result<Result<T, E>, E>` to `Result<T, E>`
+ */
+export function flattenForResult<T, E>(input: Result<Result<T, E>, E>): Result<T, E> {
+    const r: Result<T, E> = andThenForResult(input, flatten);
+    return r;
+}
