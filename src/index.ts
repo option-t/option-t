27be7ba6470dd7693d43fa8df2c0cca473bc59a6
Tetsuharu Ohzeki <tetsuharu.ohzeki@gmail.com>
2020-05-15 01:25:37 +0900

    WIP

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export * as Maybe from './Maybe/index.ts';
+export * as Nullable from './Nullable/index.ts';
+export * as PlainOption from './PlainOption/index.ts';
+export * as PlainResult from './PlainResult/index.ts';
+export * as Undefinable from './Undefinable/index.ts';
