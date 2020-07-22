1d578e3a7f5ee817a19f05b32a4d7e0558274937
Tetsuharu Ohzeki <tetsuharu.ohzeki@gmail.com>
2020-07-23 04:13:02 +0900

    WIP

--- src/.eslintrc.js
+++ src/.eslintrc.js
@@ -6,30 +6,6 @@
 // eslint-disable-next-line @typescript-eslint/no-require-imports
 const path = require('path');
 
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-const { rules } = require('../tools/eslint/eslintrc_typescript');
-const KEY_NAMING_CONVENTION = '@typescript-eslint/naming-convention';
-
-// We allow `K` or `V` forms to avoid to rewrite type parameters.
-const newNamingConventionRule = [...rules[KEY_NAMING_CONVENTION]].map((item) => {
-    if (typeof item === 'string') {
-        return item;
-    }
-
-    if (item.selector !== 'typeParameter') {
-        return item;
-    }
-
-    return {
-        'selector': 'typeParameter',
-        'format': ['PascalCase'],
-        'custom': {
-            'regex': '(^[A-Z]\\d?$|^T[A-Z][a-zA-Z]+\\d?$)',
-            'match': true,
-        },
-    };
-});
-
 // ESLint Configuration Files enables to include comments.
 // http://eslint.org/docs/configuring/#comments-in-configuration-files
 module.exports = {
@@ -49,6 +25,5 @@ module.exports = {
     },
 
     'rules': {
-        [KEY_NAMING_CONVENTION]: newNamingConventionRule,
     }
 };
