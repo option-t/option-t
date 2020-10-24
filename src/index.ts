8c5252828f9fb516c6fea9061ea7558f69b83edf
Tetsuharu Ohzeki <tetsuharu.ohzeki@gmail.com>
2020-10-25 04:14:56 +0900

    WIP

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import * as Maybe from './Maybe/index';
+import * as Nullable from './Nullable/index';
+import * as PlainOption from './PlainOption/index';
+import * as PlainResult from './PlainResult/index';
+import * as Undefinable from './Undefinable/index';
+
+export {
+    Maybe,
+    Nullable,
+    PlainOption,
+    PlainResult,
+    Undefinable,
+}
