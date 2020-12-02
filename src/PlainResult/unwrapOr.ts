ccbf9c8b06b2fb5eea5bf62ddf5f51def215955a
Tetsuharu Ohzeki <tetsuharu.ohzeki@gmail.com>
2020-12-03 02:40:09 +0900

    Omit unused generic type parameters from unwrap/unwrapErr/unwrapOr for PlainResult
    - `unknown` is _top type_. We can assign any of types to it.
    - From TS 3.5,
      [Generic type parameters are implicitly constrained to `unknown`](https://devblogs.microsoft.com/typescript/announcing-typescript-3-5/).
      Thus the behavior is not changed after this patch.
      This changes to more explicit way.
    - Ideally, we wanted to use `never` type. But `never` type is _bottom type_.
      So we cannot assign `Ok<T>` to `Ok<never>`.
    - Overloading has some problem with intellisense.
      It's not nice for productivity and this kind of troubles are happened
      in old days of rxjs.

--- src/PlainResult/unwrapOr.ts
+++ src/PlainResult/unwrapOr.ts
@@ -4,6 +4,6 @@ import { Result } from './Result';
  *  Unwraps a result _v_, returns the content of an `Ok(T)`.
  *  If the value is an `Err(E)` then return _def_.
  */
-export function unwrapOrFromResult<T, E>(v: Result<T, E>, def: T): T {
+export function unwrapOrFromResult<T>(v: Result<T, unknown>, def: T): T {
     return v.ok ? v.val : def;
 }
