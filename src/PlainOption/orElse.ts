7971fda56c7b22572b83c23622ba923a55329450
Tetsuharu Ohzeki <tetsuharu.ohzeki@gmail.com>
2020-12-16 02:44:24 +0900

    Add the test for PlainResult.orElse()

--- src/PlainOption/orElse.ts
+++ src/PlainOption/orElse.ts
@@ -5,13 +5,13 @@ export type MayRecoveryFn<T> = RecoveryFn<Option<T>>;
 
 /**
  *  Return _v_ as `T` if the passed _v_ is `Some(T)`.
- *  Otherwise, return the result of _def_.
+ *  Otherwise, return the result of _recoverer_.
  */
-export function orElseForOption<T>(v: Option<T>, def: MayRecoveryFn<T>): Option<T> {
+export function orElseForOption<T>(v: Option<T>, recoverer: MayRecoveryFn<T>): Option<T> {
     if (v.ok) {
         return v;
     } else {
-        const r = def();
+        const r = recoverer();
         return r;
     }
 }
