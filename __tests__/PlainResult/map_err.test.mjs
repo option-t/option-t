857c2e82fbb29967c49e88b4b58110923084295a
Tetsuharu Ohzeki <tetsuharu.ohzeki@gmail.com>
2020-12-16 02:26:13 +0900

    Add the test for PlainResult.mapErr()

--- __tests__/PlainResult/map_err.test.mjs
+++ __tests__/PlainResult/map_err.test.mjs
@@ -1,5 +1,36 @@
 import test from 'ava';
 
-test.todo('Ok');
+import { createOk, createErr } from '../../__dist/esm/PlainResult/Result.mjs';
+import { mapErrForResult } from '../../__dist/esm/PlainResult/mapErr.mjs';
 
-test.todo('Err');
+const VALUE_T = Math.random();
+const ERROR_E = new Error('e');
+const ERROR_F = new Error('f');
+
+test('input is Ok(T)', (t) => {
+    t.plan(3);
+
+    const input = createOk(VALUE_T);
+    const actual = mapErrForResult(input, (_) => {
+        t.pass(false);
+        return ERROR_F;
+    });
+
+    t.is(actual, input);
+    t.true(actual.ok);
+    t.is(actual.val, VALUE_T);
+});
+
+test('input is Err(E)', (t) => {
+    t.plan(4);
+
+    const input = createErr(ERROR_E);
+    const actual = mapErrForResult(input, (e) => {
+        t.is(e, ERROR_E);
+        return ERROR_F;
+    });
+
+    t.not(actual, input);
+    t.false(actual.ok);
+    t.is(actual.err, ERROR_F);
+});
