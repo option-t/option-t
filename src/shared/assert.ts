d3a1e9621336e1e238866dd61828cba0bc6ef46c
Tetsuharu Ohzeki <tetsuharu.ohzeki@gmail.com>
2021-04-22 03:06:07 +0900

    Inline isPromise()

--- src/shared/assert.ts
+++ src/shared/assert.ts
@@ -1,13 +1,8 @@
-function isPromise(input: unknown): input is Promise<unknown> {
-    const ok = input instanceof Promise;
-    return ok;
-}
-
 export function assertIsPromise(
     input: unknown,
     message: string
 ): asserts input is Promise<unknown> {
-    if (!isPromise(input)) {
+    if (!(input instanceof Promise)) {
         // We don't throw Node's AssertionError because the code size will be larger.
         throw new TypeError(message);
     }
