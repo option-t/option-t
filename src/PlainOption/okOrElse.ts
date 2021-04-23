ac4f99db03911c1b3991f62aead1645ca9fd4015
Tetsuharu Ohzeki <tetsuharu.ohzeki@gmail.com>
2021-04-23 23:42:58 +0900

    Clarify argument names again

--- src/PlainOption/okOrElse.ts
+++ src/PlainOption/okOrElse.ts
@@ -5,12 +5,15 @@ import { RecoveryFn } from '../shared/Function';
 /**
  * Transforms the `Option<T>` into a `Result<T, E>`, mapping `Some(v)` to `Ok(v)` and `None` to `Err(err())`.
  */
-export function okOrElseForPlainOption<T, E>(input: Option<T>, err: RecoveryFn<E>): Result<T, E> {
+export function okOrElseForPlainOption<T, E>(
+    input: Option<T>,
+    recoverer: RecoveryFn<E>
+): Result<T, E> {
     if (input.ok) {
         const v = createOk<T>(input.val);
         return v;
     } else {
-        const e: E = err();
+        const e: E = recoverer();
         const v = createErr<E>(e);
         return v;
     }
