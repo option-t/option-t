eecedd36a12c438aa436b6620c729b33c38d29c0
Tetsuharu Ohzeki <tetsuharu.ohzeki@gmail.com>
2021-05-07 04:31:04 +0900

    Implement `equal()` operator for PlainOption/PlainResult

---
+++ __tests__/PlainOption/equal.test.mjs
@@ -0,0 +1,69 @@
+import test from 'ava';
+
+import { createSome, createNone, isSome, isNone } from '../../__dist/esm/PlainOption/Option.mjs';
+import { equalForOption } from '../../__dist/esm/PlainOption/equal.mjs';
+import { unwrapOption } from '../../__dist/esm/PlainOption/unwrap.mjs';
+
+const INNER_VALUE_A = Symbol('A');
+const INNER_VALUE_B = Symbol('B');
+
+function toString(result) {
+    if (isSome(result)) {
+        return `Some<${String(unwrapOption(result))}>`;
+    } else if (isNone(result)) {
+        return `None`;
+    } else {
+        throw new TypeError(`unexpected type, ${String(result)}`);
+    }
+}
+
+function createTestcase(labelPrefix, lhs, rhs) {
+    const label = `${labelPrefix}: lhs(${toString(lhs)}), rhs(${toString(rhs)})`;
+    return {
+        label,
+        lhs,
+        rhs,
+    };
+}
+
+function createTestcaseForDifferentValue(lhs, rhs) {
+    return createTestcase('', lhs, rhs);
+}
+
+function createTestcaseForSameValue(lhs) {
+    return createTestcase('actual same instance', lhs, lhs);
+}
+
+{
+    // prettier-ignore
+    const trueTestCase = [
+        // Some(A)
+        [createTestcaseForDifferentValue(createSome(INNER_VALUE_A), createSome(INNER_VALUE_A)), true],
+        [createTestcaseForDifferentValue(createSome(INNER_VALUE_A), createSome(INNER_VALUE_B)), false],
+        [createTestcaseForDifferentValue(createSome(INNER_VALUE_A), createNone()), false],
+
+        // Some(B)
+        [createTestcaseForDifferentValue(createSome(INNER_VALUE_B), createSome(INNER_VALUE_A)), false],
+        [createTestcaseForDifferentValue(createSome(INNER_VALUE_B), createSome(INNER_VALUE_B)), true],
+        [createTestcaseForDifferentValue(createSome(INNER_VALUE_B), createNone()), false],
+
+        // None
+        [createTestcaseForDifferentValue(createNone(), createSome(INNER_VALUE_A)), false],
+        [createTestcaseForDifferentValue(createNone(), createSome(INNER_VALUE_B)), false],
+        [createTestcaseForDifferentValue(createNone(), createNone()), true],
+
+        // actually same instance
+        [createTestcaseForSameValue(createSome(INNER_VALUE_A)), true],
+        [createTestcaseForSameValue(createNone()), true],
+    ];
+
+    for (const [{ label, lhs, rhs }, expected] of trueTestCase) {
+        test(label + ', lhs == rhs', (t) => {
+            t.is(equalForOption(lhs, rhs), expected);
+        });
+
+        test(label + ', rhs == lhs', (t) => {
+            t.is(equalForOption(rhs, lhs), expected);
+        });
+    }
+}
---
+++ src/PlainOption/equal.ts
@@ -0,0 +1,25 @@
+import type { Option } from './Option';
+
+/**
+ *  Return `true`:
+ *      1. if `lhs === rhs`.
+ *      2. if _lhs_ and _rhs_ is same kind, and they contain same value.
+ *
+ *  Otherwise, return `false`.
+ *
+ *  This function is designed for `Option<T>`.
+ *  This function may return `true` if input values has same properties with `Option<T>`
+ *  and their properties are same between _lhs_ and _rhs_.
+ */
+export function equalForOption<T>(lhs: Option<T>, rhs: Option<T>): boolean {
+    if (lhs === rhs) {
+        return true;
+    }
+
+    if (lhs.ok !== rhs.ok) {
+        return false;
+    }
+
+    const isEqual = lhs.val === rhs.val;
+    return isEqual;
+}
