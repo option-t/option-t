5b6aaea2f86fec1602fc9c0d889908e97ba8b8da
Tetsuharu Ohzeki <tetsuharu.ohzeki@gmail.com>
2021-05-15 03:16:34 +0900

    Implement mapAsync for Undefinable

---
+++ __tests__/Undefinable/map_async.test.mjs
@@ -0,0 +1,94 @@
+import test from 'ava';
+
+import { mapAsyncForUndefinable } from '../../__dist/esm/Undefinable/mapAsync.mjs';
+import { nonNullableValue } from '../utils.mjs';
+
+const NULL_VALUE_IN_THIS_TEST_CASE = undefined;
+const NULLY_VALUE_BUT_NOT_NULL_VALUE_IN_THIS_TEST_CASE = null;
+
+const nonNullableValueCaseList = nonNullableValue.map((input) => {
+    if (input instanceof Promise) {
+        // Promise will be unwrap recursively automatically.
+        // There is no ways to unwrap single level now.
+        // So we need to care that.
+        return [input, input, 'this is promise'];
+    }
+
+    return [input, input, input];
+});
+
+for (const [INPUT, PASSED_EXPECTED, FINAL_EXPECTED] of nonNullableValueCaseList) {
+    test('pass the value:  ' + String(INPUT), async (t) => {
+        t.plan(4);
+
+        const result = mapAsyncForUndefinable(INPUT, async (v) => {
+            t.pass();
+            t.is(v, PASSED_EXPECTED, 'the arg is the input');
+            return v;
+        });
+
+        t.true(result instanceof Promise, 'result should be Promise');
+        const actual = await result;
+        t.is(actual, FINAL_EXPECTED);
+    });
+}
+
+test(`pass ${NULL_VALUE_IN_THIS_TEST_CASE}`, async (t) => {
+    t.plan(2);
+
+    const result = mapAsyncForUndefinable(NULL_VALUE_IN_THIS_TEST_CASE, async (_v) => {
+        t.fail('do not call this');
+    });
+
+    t.true(result instanceof Promise, 'result should be Promise');
+    const actual = await result;
+    t.is(actual, NULL_VALUE_IN_THIS_TEST_CASE);
+});
+
+test(`pass ${NULLY_VALUE_BUT_NOT_NULL_VALUE_IN_THIS_TEST_CASE}`, async (t) => {
+    t.plan(3);
+
+    const result = mapAsyncForUndefinable(
+        NULLY_VALUE_BUT_NOT_NULL_VALUE_IN_THIS_TEST_CASE,
+        async (v) => {
+            t.pass();
+            return v;
+        }
+    );
+
+    t.true(result instanceof Promise, 'result should be Promise');
+    const actual = await result;
+    t.is(actual, NULLY_VALUE_BUT_NOT_NULL_VALUE_IN_THIS_TEST_CASE);
+});
+
+const testcases = [[1, NULL_VALUE_IN_THIS_TEST_CASE]];
+for (const [src, def] of testcases) {
+    test(`assert that do not return Undefinable<*> as the selector's result, v = ${String(
+        src
+    )}, def = ${String(def)}`, async (t) => {
+        await t.throwsAsync(
+            async () => {
+                mapAsyncForUndefinable(src, (_v) => def);
+                t.fail('do not reach to here');
+            },
+            { instanceOf: TypeError }
+        );
+    });
+}
+
+test('callback should return Promise', async (t) => {
+    t.plan(2);
+
+    await t.throwsAsync(
+        async () => {
+            await mapAsyncForUndefinable(NULLY_VALUE_BUT_NOT_NULL_VALUE_IN_THIS_TEST_CASE, () => {
+                t.pass();
+                return 1;
+            });
+        },
+        {
+            instanceOf: TypeError,
+            message: '`transformer` must return Promise',
+        }
+    );
+});
