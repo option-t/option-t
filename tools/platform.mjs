763e0432af4a440b7628b23512325f56d7b8eb86
Tetsuharu Ohzeki <tetsuharu.ohzeki@gmail.com>
2021-06-06 03:23:03 +0900

    Remove unused files

--- tools/platform.mjs
+++
@@ -1,7 +0,0 @@
-import * as os from 'os';
-
-export function getSuffixedCommandName(command) {
-    const suffix = (os.platform() === 'win32') ? '.cmd' : '';
-    const name = command + suffix;
-    return name;
-}
