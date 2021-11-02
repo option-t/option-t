7ecdb8090f7d56ec2091f94da26707d2b1ac1c75
Tetsuharu Ohzeki <tetsuharu.ohzeki@gmail.com>
2021-11-03 00:09:27 +0900

    Switch to ava v4 rc
    https://github.com/avajs/ava/releases/tag/v4.0.0-rc.1

--- ava.config.mjs
+++ ava.config.mjs
@@ -1,14 +1,5 @@
 export default function resolveAvaConfig() {
     return {
-        nonSemVerExperiments: {
-            nextGenConfig: true,
-        },
-
         files: ['__tests__/**/*.test.mjs'],
-        babel: {
-            testOptions: {
-                babelrc: false,
-            },
-        },
     };
 }
