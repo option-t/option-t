2cfc1dc14c230de0c01017740e07f658be8d11d4
Tetsuharu Ohzeki <tetsuharu.ohzeki@gmail.com>
2021-11-09 06:14:17 +0900

    Decorate all symbols in ./shared/Function

--- src/shared/Function.ts
+++ src/shared/Function.ts
@@ -2,16 +2,68 @@
  *  @deprecated
  *  This module is for backward compatibility.
  */
-export type {
-    TransformFn,
-    RecoveryFn,
-    RecoveryFromErrorFn,
-    EffectFn,
-    FilterFn,
-    AsyncTransformFn,
-    AsyncRecoveryFn,
-    AsyncRecoveryFromErrorFn,
-    MapFn,
-    RecoveryWithErrorFn,
-    TapFn,
+import type {
+    TransformFn as InternalTransformFn,
+    RecoveryFn as InternalRecoveryFn,
+    RecoveryFromErrorFn as InternalRecoveryFromErrorFn,
+    EffectFn as InternalEffectFn,
+    FilterFn as InternalFilterFn,
+    AsyncTransformFn as InternalAsyncTransformFn,
+    AsyncRecoveryFn as InternalAsyncRecoveryFn,
+    AsyncRecoveryFromErrorFn as InternalAsyncRecoveryFromErrorFn,
 } from '../internal/Function';
+
+/**
+ *  @deprecated This type is provided for backward compatibility.
+ */
+export type TransformFn<T, U> = InternalTransformFn<T, U>;
+
+/**
+ *  @deprecated This type is provided for backward compatibility.
+ */
+export type RecoveryFn<T> = InternalRecoveryFn<T>;
+
+/**
+ *  @deprecated This type is provided for backward compatibility.
+ */
+export type RecoveryFromErrorFn<E, T> = InternalRecoveryFromErrorFn<E, T>;
+
+/**
+ *  @deprecated This type is provided for backward compatibility.
+ */
+export type EffectFn<T> = InternalEffectFn<T>;
+
+/**
+ *  @deprecated This type is provided for backward compatibility.
+ */
+export type FilterFn<T> = InternalFilterFn<T>;
+
+/**
+ *  @deprecated This type is provided for backward compatibility.
+ */
+export type AsyncTransformFn<T, U> = InternalAsyncTransformFn<T, U>;
+
+/**
+ *  @deprecated This type is provided for backward compatibility.
+ */
+export type AsyncRecoveryFn<T> = InternalAsyncRecoveryFn<T>;
+
+/**
+ *  @deprecated This type is provided for backward compatibility.
+ */
+export type AsyncRecoveryFromErrorFn<E, T> = InternalAsyncRecoveryFromErrorFn<E, T>;
+
+/**
+ *  @deprecated Use {@link TransformFn} in the same module.
+ */
+export type MapFn<T, U> = TransformFn<T, U>;
+
+/**
+ *  @deprecated Use {@link RecoveryWithErrorFn} in the same module.
+ */
+export type RecoveryWithErrorFn<E, T> = RecoveryFromErrorFn<E, T>;
+
+/**
+ *  @deprecated Use {@link EffectFn} in the same module.
+ */
+export type TapFn<T> = EffectFn<T>;
