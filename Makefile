option-t/option-t

commit e3601aa4ffa3b6468eccbff497c31d20539ad94a

Use `d.cts` as an extension for type definition of cjs files
We need to use `d.cts` for cjs file if package.json's `type` is `module`.

Makefile
@@ -78,15 +78,19 @@ build: __build cp_docs cp_changelog cp_license cp_readme generate_manifest ## Bu
 __build: build_cjs build_esm build_mixedlib ## Build all targets.
 
 .PHONY: build_cjs
-build_cjs: __build_cjs__cp_cjs_to_cjsdir __build_cjs__cp_dts_to_cjsdir ## Build `cjs/`.
+build_cjs: __build_cjs__cp_cjs_to_cjsdir __build_cjs__cp_dcts_to_cjsdir ## Build `cjs/`.
 
 .PHONY: __build_cjs__cp_cjs_to_cjsdir
 __build_cjs__cp_cjs_to_cjsdir: __build_cjs__rename_js_to_cjs clean_dist
 	$(NPM_BIN)/babel $(TMP_CJS_DIR) --out-dir $(DIST_COMMONJS_DIR) --extensions=.cjs --no-babelrc --config-file $(CURDIR)/tools/babel/babelrc.cjs.mjs --keep-file-extension
 
-.PHONY: build_mjs__cp_dts_to_cjsdir
-__build_cjs__cp_dts_to_cjsdir: __build_cjs__create_tmp_cjs clean_dist
+.PHONY: __build_cjs__cp_dcts_to_cjsdir
+__build_cjs__cp_dcts_to_cjsdir: __build_cjs_rename_dts_to_dcts clean_dist
 	$(NODE_BIN) $(CURDIR)/tools/cp_files.mjs --basedir $(TMP_CJS_DIR) --source '$(TMP_CJS_DIR)/**/*.$(DTS_EXTENSION_GLOB)' --destination $(DIST_COMMONJS_DIR)
+
+.PHONY: __build_cjs_rename_dts_to_dcts
+__build_cjs_rename_dts_to_dcts: __build_cjs__create_tmp_cjs
+	$(NODE_BIN) $(CURDIR)/tools/extension_renamer.mjs --target-dir $(TMP_CJS_DIR) --to-extension 'cts' --from-extension 'ts'
 
 .PHONY: __build_cjs__rename_js_to_cjs
 __build_cjs__rename_js_to_cjs: __build_cjs__create_tmp_cjs

tools/package_json_rewriter/transformer/add_exports_field/ExportEntry.mjs
@@ -16,6 +16,7 @@ class AbstractExportEntry {
 const EXTENSION_CJS = 'cjs';
 const EXTENSION_MJS = 'mjs';
 const EXTENSION_DTS = 'd.ts';
+const EXTENSION_DCTS = 'd.cts';
 
 export class ExportEntry extends AbstractExportEntry {
     #key;
@@ -113,7 +114,7 @@ class CommonJSCompatFileExport extends CompatExportEntry {
 
     _toExportEntry() {
         const key = this.key();
-        const dts = `${key}.${EXTENSION_DTS}`;
+        const dts = `${key}.${EXTENSION_DCTS}`;
 
         const p = `${key}.${EXTENSION_CJS}`;
         const value = constructPathValue({
@@ -181,11 +182,13 @@ class LibCompatFileExport extends CompatExportEntry {
 class AbstractCompatDirExport extends AbstractExportEntry {
     #dirpath;
     #extension;
+    #dtsExtension;
 
-    constructor(dirpath, ext) {
+    constructor(dirpath, ext, dtsExt) {
         super();
         this.#dirpath = dirpath;
         this.#extension = ext;
+        this.#dtsExtension = dtsExt;
     }
 
     key() {
@@ -198,7 +201,8 @@ class AbstractCompatDirExport extends AbstractExportEntry {
         const filepath = `${key}/index`;
         const ext = this.#extension;
         const fullpath = `${filepath}.${ext}`;
-        const dts = `${filepath}.${EXTENSION_DTS}`;
+        const dtsExt = this.#dtsExtension;
+        const dts = `${filepath}.${dtsExt}`;
 
         const value = constructPathValue({
             dts,
@@ -215,7 +219,7 @@ class AbstractCompatDirExport extends AbstractExportEntry {
 export class CommonJSCompatDirExport extends AbstractCompatDirExport {
     constructor(dirpath) {
         const p = `cjs/${dirpath}`;
-        super(p, EXTENSION_CJS);
+        super(p, EXTENSION_CJS, EXTENSION_DCTS);
         Object.freeze(this);
     }
 }
@@ -223,7 +227,7 @@ export class CommonJSCompatDirExport extends AbstractCompatDirExport {
 export class ESModuleCompatDirExport extends AbstractCompatDirExport {
     constructor(dirpath) {
         const p = `esm/${dirpath}`;
-        super(p, EXTENSION_MJS);
+        super(p, EXTENSION_MJS, EXTENSION_DTS);
         Object.freeze(this);
     }
 }

commit 9ae75978b7e666b7a87f4400dafe94b08e0ff7ba

Fix to import `.d.cts` from other `d.cts`
This fix a regression since e3601aa4ffa3b6468eccbff497c31d20539ad94a

Makefile
@@ -87,7 +87,7 @@ __build_cjs__cp_cjs_to_cjsdir: __build_cjs__rename_js_to_cjs clean_dist
 
 .PHONY: __build_cjs__cp_dcts_to_cjsdir
 __build_cjs__cp_dcts_to_cjsdir: __build_cjs_rename_dts_to_dcts clean_dist
-	$(NODE_BIN) $(CURDIR)/tools/cp_files.mjs --basedir $(TMP_CJS_DIR) --source '$(TMP_CJS_DIR)/**/*.$(DTS_EXTENSION_GLOB)' --destination $(DIST_COMMONJS_DIR)
+	$(NPM_BIN)/babel $(TMP_CJS_DIR) --out-dir $(DIST_COMMONJS_DIR) --extensions=.cts --no-babelrc --config-file $(CURDIR)/tools/babel/babelrc.d.cts.mjs --keep-file-extension
 
 .PHONY: __build_cjs_rename_dts_to_dcts
 __build_cjs_rename_dts_to_dcts: __build_cjs__create_tmp_cjs

tools/babel/babelrc.d.cts.mjs
@@ -1,0 +1,10 @@
+export default {
+    'plugins': [
+        ['@babel/plugin-syntax-typescript', {
+            dts: true,
+        }],
+        ['./babel-plugin-modify-ext.mjs', {
+            extension: '.cjs',
+        }],
+    ],
+};