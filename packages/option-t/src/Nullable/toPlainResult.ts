acc910e2ca310c43d912a182d3205d2986dacc1f
Tetsuharu Ohzeki <tetsuharu.ohzeki@gmail.com>
2023-02-07 14:40:45 +0900

    Add jsdoc for toPlainResult for Nullable

--- packages/option-t/src/Nullable/toPlainResult.ts
+++ packages/option-t/src/Nullable/toPlainResult.ts
@@ -1,6 +1,10 @@
 import { type Result, type Ok, type Err, createOk, createErr } from '../PlainResult/Result.js';
 import { isNull, type Nullable } from './Nullable.js';
 
+/**
+ *  Return `Err<void>` if _input_ is `null`.
+ *  Otherwise, return `Ok<T>` directly.
+ */
 export function toResultOkFromNullable<T>(input: Nullable<T>): Result<T, void> {
     if (isNull(input)) {
         return createErr(undefined);
@@ -10,6 +14,10 @@ export function toResultOkFromNullable<T>(input: Nullable<T>): Result<T, void> {
     return result;
 }
 
+/**
+ *  Return `Ok<void>` if _input_ is `null`.
+ *  Otherwise, return `Err<E>` directly.
+ */
 export function toResultErrFromNullable<E>(input: Nullable<E>): Result<void, E> {
     if (isNull(input)) {
         return createOk(undefined);
