9299b9edd8758ef2cddb1e648810f9077f402a76
Tetsuharu Ohzeki <tetsuharu.ohzeki@gmail.com>
2023-02-07 13:11:22 +0900

    Implement `toUndefinable()` for `PlainResult`

---
+++ packages/option-t/src/PlainResult/toUndefinable.ts
@@ -0,0 +1,21 @@
+import type { Undefinable } from '../Undefinable/Undefinable.js';
+import type { Result } from './Result.js';
+import { unwrapOrFromResult, unwrapErrOrFromResult } from './unwrapOr.js';
+
+/**
+ *  Unwrap `T` if _input_ is `Ok(T)`.
+ *  Otherwise, return `undefined`.
+ */
+export function fromOkToUndefinable<T>(input: Result<T, unknown>): Undefinable<T> {
+    const val: Undefinable<T> = unwrapOrFromResult<Undefinable<T>>(input, undefined);
+    return val;
+}
+
+/**
+ *  Unwrap `E` if _input_ is `Err(E)`.
+ *  Otherwise, return `undefined`.
+ */
+export function fromErrToUndefinable<E>(input: Result<unknown, E>): Undefinable<E> {
+    const err: Undefinable<E> = unwrapErrOrFromResult<Undefinable<E>>(input, undefined);
+    return err;
+}
