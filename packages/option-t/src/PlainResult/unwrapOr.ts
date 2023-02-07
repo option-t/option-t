edd73436acb5a7498b4884330152ecbf0381e85c
Tetsuharu Ohzeki <tetsuharu.ohzeki@gmail.com>
2023-02-07 12:59:50 +0900

    Implement `toNullable()` for `PlainResult`
    To implement this simply, we also implement `unwrapErrOrFromResult()`

--- packages/option-t/src/PlainResult/unwrapOr.ts
+++ packages/option-t/src/PlainResult/unwrapOr.ts
@@ -1,4 +1,4 @@
-import { Result } from './Result.js';
+import { type Result, isOk } from './Result.js';
 
 /**
  *  Unwraps a result _input_, returns the content of an `Ok(T)`.
@@ -11,4 +11,17 @@ export function unwrapOrFromResult<T>(input: Result<T, unknown>, defaultValue: T
     }
 
     return defaultValue;
+}
+
+/**
+ *  Unwraps a result _input_, returns the content of an `Err(E)`.
+ *  If the value is an `Ok(T)` then return _defaultValue_.
+ */
+export function unwrapErrOrFromResult<E>(input: Result<unknown, E>, defaultValue: E): E {
+    if (isOk(input)) {
+        return defaultValue;
+    }
+
+    const val: E = input.err;
+    return val;
 }
