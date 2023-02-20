1dabfd7cb635442c623d41ac8dfeaabd425f209a
Tetsuharu Ohzeki <tetsuharu.ohzeki@gmail.com>
2023-02-20 23:56:06 +0900

    Mark deprecated API which user should replace when they stop to use a compat path

--- packages/option-t/src/PlainResult/compat/v33.ts
+++ packages/option-t/src/PlainResult/compat/v33.ts
@@ -17,94 +17,248 @@ export {
     expectErr,
 } from '../Result.js';
 
-export { andForResult, andForResult as and } from '../and.js';
-export { andThenForResult, andThenForResult as andThen } from '../andThen.js';
-export { andThenAsyncForResult, andThenAsyncForResult as andThenAsync } from '../andThenAsync.js';
-export { equalForResult, equalForResult as equal } from '../equal.js';
-export { flattenForResult, flattenForResult as flatten } from '../flatten.js';
-export {
-    inspectOkOfResult,
-    inspectErrOfResult,
-    inspectBothOfResult,
-    inspectOkOfResult as inspectOk,
-    inspectErrOfResult as inspectErr,
-    inspectBothOfResult as inspectBoth,
-} from '../inspect.js';
-export { mapForResult, mapForResult as map } from '../map.js';
-export { mapAsyncForResult, mapAsyncForResult as mapAsync } from '../mapAsync.js';
-export { mapOrForResult, mapOrForResult as mapOr } from '../mapOr.js';
-export { mapOrAsyncForResult, mapOrAsyncForResult as mapOrAsync } from '../mapOrAsync.js';
-export { mapOrElseForResult, mapOrElseForResult as mapOrElse } from '../mapOrElse.js';
-export {
-    mapOrElseAsyncForResult,
-    mapOrElseAsyncForResult as mapOrElseAsync,
-} from '../mapOrElseAsync.js';
-export { mapErrForResult, mapErrForResult as mapErr } from '../mapErr.js';
-export { mapErrAsyncForResult, mapErrAsyncForResult as mapErrAsync } from '../mapErrAsync.js';
-export { orForResult, orForResult as or } from '../or.js';
-export { orElseForResult, orElseForResult as orElse } from '../orElse.js';
-export { orElseAsyncForResult, orElseAsyncForResult as orElseAsync } from '../orElseAsync.js';
-export {
-    transposeForResult,
-    transposeNullableForResult,
-    transposeUndefinableForResult,
-    transposeForResult as transpose,
-    transposeNullableForResult as transposeNullable,
-    transposeUndefinableForResult as transposeUndefinable,
-} from '../transpose.js';
-export { unwrapOrFromResult, unwrapOrFromResult as unwrapOr } from '../unwrapOr.js';
-export { unwrapOrElseFromResult, unwrapOrElseFromResult as unwrapOrElse } from '../unwrapOrElse.js';
-export {
-    unwrapOrElseAsyncFromResult,
-    unwrapOrElseAsyncFromResult as unwrapOrElseAsync,
-} from '../unwrapOrElseAsync.js';
+export { andForResult } from '../and.js';
+export { andThenForResult } from '../andThen.js';
+export { andThenAsyncForResult } from '../andThenAsync.js';
+export { equalForResult } from '../equal.js';
+export { flattenForResult } from '../flatten.js';
+export { inspectOkOfResult, inspectErrOfResult, inspectBothOfResult } from '../inspect.js';
+export { mapForResult } from '../map.js';
+export { mapAsyncForResult } from '../mapAsync.js';
+export { mapOrForResult } from '../mapOr.js';
+export { mapOrAsyncForResult } from '../mapOrAsync.js';
+export { mapOrElseForResult } from '../mapOrElse.js';
+export { mapOrElseAsyncForResult } from '../mapOrElseAsync.js';
+export { mapErrForResult } from '../mapErr.js';
+export { mapErrAsyncForResult } from '../mapErrAsync.js';
+export { orForResult } from '../or.js';
+export { orElseForResult } from '../orElse.js';
+export { orElseAsyncForResult } from '../orElseAsync.js';
+export { transposeNullableForResult, transposeUndefinableForResult } from '../transpose.js';
+export { unwrapOrFromResult } from '../unwrapOr.js';
+export { unwrapOrElseFromResult } from '../unwrapOrElse.js';
+export { unwrapOrElseAsyncFromResult } from '../unwrapOrElseAsync.js';
 
 import { expectOk, expectErr, unwrapOk } from '../Result.js';
 import {
     toOptionFromOk as toOptionFromOkFn,
     toOptionFromErr as toOptionFromErrFn,
 } from '../toOption.js';
+import { andForResult } from '../and.js';
+import { andThenForResult } from '../andThen.js';
+import { andThenAsyncForResult } from '../andThenAsync.js';
+import { equalForResult } from '../equal.js';
+import { flattenForResult } from '../flatten.js';
+import { inspectOkOfResult, inspectErrOfResult, inspectBothOfResult } from '../inspect.js';
+import { mapForResult } from '../map.js';
+import { mapAsyncForResult } from '../mapAsync.js';
+import { mapOrForResult } from '../mapOr.js';
+import { mapOrAsyncForResult } from '../mapOrAsync.js';
+import { mapOrElseForResult } from '../mapOrElse.js';
+import { mapOrElseAsyncForResult } from '../mapOrElseAsync.js';
+import { mapErrForResult } from '../mapErr.js';
+import { mapErrAsyncForResult } from '../mapErrAsync.js';
+import { orForResult } from '../or.js';
+import { orElseForResult } from '../orElse.js';
+import { orElseAsyncForResult } from '../orElseAsync.js';
+import {
+    transposeForResult as transposeForResultOriginal,
+    transposeNullableForResult,
+    transposeUndefinableForResult,
+} from '../transpose.js';
+import { unwrapOrFromResult } from '../unwrapOr.js';
+import { unwrapOrElseFromResult } from '../unwrapOrElse.js';
+import { unwrapOrElseAsyncFromResult } from '../unwrapOrElseAsync.js';
 
 /**
  *  @deprecated
- *  This is an alias for backward compatibility.
- *  Please use {@link expectOk} instead.
+ *  Please use {@link expectOk} in `option-t/PlainResult/Result` or `option-t/PlainResult`.
  */
 export const expect: typeof expectOk = expectOk;
 
 /**
  *  @deprecated
- *  This is an alias for backward compatibility.
- *  Please use {@link expectOk} instead.
+ *  Please use {@link expectOk} in `option-t/PlainResult/Result` or `option-t/PlainResult`.
  */
 export const expectIsOk: typeof expectOk = expectOk;
 
 /**
  *  @deprecated
- *  This is an alias for backward compatibility.
- *  Please use {@link expectErr} instead.
+ *  Please use {@link expectErr} in `option-t/PlainResult/Result` or `option-t/PlainResult`.
  */
 export const expectIsErr: typeof expectErr = expectErr;
 
 /**
  *  @deprecated
- *  This is an alias for backward compatibility.
- *  Please use {@link unwrapOk} instead.
+ *  Please use {@link unwrapOk} in `option-t/PlainResult/Result` or `option-t/PlainResult`.
  */
 export const unwrap: typeof unwrapOk = unwrapOk;
 
 /**
  *  @deprecated
- *  This is an alias for backward compatibility.
- *  __But this will be removed in the future by deprecating `PlainOption`.__
- *  Please import from `./PlainResult/toOption` instead.
+ *  Please use {@link toOptionFromOkFn} in `option-t/PlainResult/toOption`.
  */
 export const toOptionFromOk: typeof toOptionFromOkFn = toOptionFromOkFn;
 
 /**
  *  @deprecated
- *  This is an alias for backward compatibility.
- *  __But this will be removed in the future by deprecating `PlainOption`.__
- *  Please import from `./PlainResult/toOption` instead.
+ *  Please use {@link toOptionFromErrFn} in `option-t/PlainResult/toOption`.
  */
 export const toOptionFromErr: typeof toOptionFromErrFn = toOptionFromErrFn;
+
+/**
+ *  @deprecated
+ *  Please use {@link andForResult} in `option-t/PlainResult/and`.
+ */
+export const and: typeof andForResult = andForResult;
+
+/**
+ *  @deprecated
+ *  Please use {@link andThenForResult} in `option-t/PlainResult/andThen`.
+ */
+export const andThen: typeof andThenForResult = andThenForResult;
+
+/**
+ *  @deprecated
+ *  Please use {@link andThenAsyncForResult} in `option-t/PlainResult/andThenAsync`.
+ */
+export const andThenAsync: typeof andThenAsyncForResult = andThenAsyncForResult;
+
+/**
+ *  @deprecated
+ *  Please use {@link equalForResult} in `option-t/PlainResult/equal`.
+ */
+export const equal: typeof equalForResult = equalForResult;
+
+/**
+ *  @deprecated
+ *  Please use {@link flattenForResult} in `option-t/PlainResult/flatten` or `option-t/PlainResult`.
+ */
+export const flatten: typeof flattenForResult = flattenForResult;
+
+/**
+ *  @deprecated
+ *  Please use {@link inspectOkOfResult} in `option-t/PlainResult/inspect` or `option-t/PlainResult`.
+ */
+export const inspectOk: typeof inspectOkOfResult = inspectOkOfResult;
+
+/**
+ *  @deprecated
+ *  Please use {@link inspectErrOfResult} in `option-t/PlainResult/inspect` or `option-t/PlainResult`.
+ */
+export const inspectErr: typeof inspectErrOfResult = inspectErrOfResult;
+
+/**
+ *  @deprecated
+ *  Please use {@link inspectBothOfResult} in `option-t/PlainResult/inspect` or `option-t/PlainResult`.
+ */
+export const inspectBoth: typeof inspectBothOfResult = inspectBothOfResult;
+
+/**
+ *  @deprecated
+ *  Please use {@link mapForResult} in `option-t/PlainResult/map` or `option-t/PlainResult`.
+ */
+export const map: typeof mapForResult = mapForResult;
+
+/**
+ *  @deprecated
+ *  Please use {@link mapAsyncForResult} in `option-t/PlainResult/mapAsync` or `option-t/PlainResult`.
+ */
+export const mapAsync: typeof mapAsyncForResult = mapAsyncForResult;
+
+/**
+ *  @deprecated
+ *  Please use {@link mapOrForResult} in `option-t/PlainResult/mapOr` or `option-t/PlainResult`.
+ */
+export const mapOr: typeof mapOrForResult = mapOrForResult;
+
+/**
+ *  @deprecated
+ *  Please use {@link mapOrAsyncForResult} in `option-t/PlainResult/mapOrAsync` or `option-t/PlainResult`.
+ */
+export const mapOrAsync: typeof mapOrAsyncForResult = mapOrAsyncForResult;
+
+/**
+ *  @deprecated
+ *  Please use {@link mapOrElseForResult} in `option-t/PlainResult/mapOrElse` or `option-t/PlainResult`.
+ */
+export const mapOrElse: typeof mapOrElseForResult = mapOrElseForResult;
+
+/**
+ *  @deprecated
+ *  Please use {@link mapOrElseAsyncForResult} in `option-t/PlainResult/mapOrElseAsync` or `option-t/PlainResult`.
+ */
+export const mapOrElseAsync: typeof mapOrElseAsyncForResult = mapOrElseAsyncForResult;
+
+/**
+ *  @deprecated
+ *  Please use {@link mapErrForResult} in `option-t/PlainResult/mapErr` or `option-t/PlainResult`.
+ */
+export const mapErr: typeof mapErrForResult = mapErrForResult;
+
+/**
+ *  @deprecated
+ *  Please use {@link mapErrAsyncForResult} in `option-t/PlainResult/mapErrAsync` or `option-t/PlainResult`.
+ */
+export const mapErrAsync: typeof mapErrAsyncForResult = mapErrAsyncForResult;
+
+/**
+ *  @deprecated
+ *  Please use {@link orForResult} in `option-t/PlainResult/or`.
+ */
+export const or: typeof orForResult = orForResult;
+
+/**
+ *  @deprecated
+ *  Please use {@link orElseForResult} in `option-t/PlainResult/orElse` or `option-t/PlainResult`.
+ */
+export const orElse: typeof orElseForResult = orElseForResult;
+
+/**
+ *  @deprecated
+ *  Please use {@link orElseAsyncForResult} in `option-t/PlainResult/orElseAsync` or `option-t/PlainResult`.
+ */
+export const orElseAsync: typeof orElseAsyncForResult = orElseAsyncForResult;
+
+/**
+ *  @deprecated
+ *  Please use {@link transposeForResult} in `option-t/PlainResult/transpose`.
+ */
+export const transposeForResult: typeof transposeForResultOriginal = transposeForResultOriginal;
+
+/**
+ *  @deprecated
+ *  Please use {@link transposeForResult} in `option-t/PlainResult/transpose`.
+ */
+export const transpose: typeof transposeForResult = transposeForResult;
+
+/**
+ *  @deprecated
+ *  Please use {@link transposeNullableForResult} in `option-t/PlainResult/transpose` or `option-t/PlainResult`.
+ */
+export const transposeNullable: typeof transposeNullableForResult = transposeNullableForResult;
+
+/**
+ *  @deprecated
+ *  Please use {@link transposeUndefinableForResult} in `option-t/PlainResult/transpose` or `option-t/PlainResult`.
+ */
+export const transposeUndefinable: typeof transposeUndefinableForResult =
+    transposeUndefinableForResult;
+
+/**
+ *  @deprecated
+ *  Please use {@link unwrapOrFromResult} in `option-t/PlainResult/unwrapOr` or `option-t/PlainResult`.
+ */
+export const unwrapOr: typeof unwrapOrFromResult = unwrapOrFromResult;
+
+/**
+ *  @deprecated
+ *  Please use {@link unwrapOrElseFromResult} in `option-t/PlainResult/unwrapOrElse` or `option-t/PlainResult`.
+ */
+export const unwrapOrElse: typeof unwrapOrElseFromResult = unwrapOrElseFromResult;
+
+/**
+ *  @deprecated
+ *  Please use {@link unwrapOrElseAsyncFromResult} in `option-t/PlainResult/unwrapOrElseAsync` or `option-t/PlainResult`.
+ */
+export const unwrapOrElseAsync: typeof unwrapOrElseAsyncFromResult = unwrapOrElseAsyncFromResult;
--- packages/option-t/src/Undefinable/compat/v33.ts
+++ packages/option-t/src/Undefinable/compat/v33.ts
@@ -11,49 +11,130 @@ export {
     expectNotUndefined,
     unwrapUndefinable,
 } from '../Undefinable.js';
-
-export { andThenForUndefinable, andThenForUndefinable as andThen } from '../andThen.js';
-export {
-    andThenAsyncForUndefinable,
-    andThenAsyncForUndefinable as andThenAsync,
-} from '../andThenAsync.js';
-export { inspectUndefinable, inspectUndefinable as inspect } from '../inspect.js';
-export { mapForUndefinable, mapForUndefinable as map } from '../map.js';
-export { mapAsyncForUndefinable, mapAsyncForUndefinable as mapAsync } from '../mapAsync.js';
-export { mapOrForUndefinable, mapOrForUndefinable as mapOr } from '../mapOr.js';
-export { mapOrAsyncForUndefinable, mapOrAsyncForUndefinable as mapOrAsync } from '../mapOrAsync.js';
-export { mapOrElseForUndefinable, mapOrElseForUndefinable as mapOrElse } from '../mapOrElse.js';
-export {
-    mapOrElseAsyncForUndefinable,
-    mapOrElseAsyncForUndefinable as mapOrElseAsync,
-} from '../mapOrElseAsync.js';
-export { orElseForUndefinable, orElseForUndefinable as orElse } from '../orElse.js';
-export {
-    orElseAsyncForUndefinable,
-    orElseAsyncForUndefinable as orElseAsync,
-} from '../orElseAsync.js';
-export { unwrapOrFromUndefinable, unwrapOrFromUndefinable as unwrapOr } from '../unwrapOr.js';
-export {
-    unwrapOrElseFromUndefinable,
-    unwrapOrElseFromUndefinable as unwrapOrElse,
-} from '../unwrapOrElse.js';
-export {
-    unwrapOrElseAsyncFromUndefinable,
-    unwrapOrElseAsyncFromUndefinable as unwrapOrElseAsync,
-} from '../unwrapOrElseAsync.js';
+export { andThenForUndefinable } from '../andThen.js';
+export { andThenAsyncForUndefinable } from '../andThenAsync.js';
+export { inspectUndefinable } from '../inspect.js';
+export { mapForUndefinable } from '../map.js';
+export { mapAsyncForUndefinable } from '../mapAsync.js';
+export { mapOrForUndefinable } from '../mapOr.js';
+export { mapOrAsyncForUndefinable } from '../mapOrAsync.js';
+export { mapOrElseForUndefinable } from '../mapOrElse.js';
+export { mapOrElseAsyncForUndefinable } from '../mapOrElseAsync.js';
+export { orElseForUndefinable } from '../orElse.js';
+export { orElseAsyncForUndefinable } from '../orElseAsync.js';
+export { unwrapOrFromUndefinable } from '../unwrapOr.js';
+export { unwrapOrElseFromUndefinable } from '../unwrapOrElse.js';
+export { unwrapOrElseAsyncFromUndefinable } from '../unwrapOrElseAsync.js';
 
 import { expectNotUndefined, unwrapUndefinable } from '../Undefinable.js';
+import { andThenForUndefinable } from '../andThen.js';
+import { andThenAsyncForUndefinable } from '../andThenAsync.js';
+import { inspectUndefinable } from '../inspect.js';
+import { mapForUndefinable } from '../map.js';
+import { mapAsyncForUndefinable } from '../mapAsync.js';
+import { mapOrForUndefinable } from '../mapOr.js';
+import { mapOrAsyncForUndefinable } from '../mapOrAsync.js';
+import { mapOrElseForUndefinable } from '../mapOrElse.js';
+import { mapOrElseAsyncForUndefinable } from '../mapOrElseAsync.js';
+import { orElseForUndefinable } from '../orElse.js';
+import { orElseAsyncForUndefinable } from '../orElseAsync.js';
+import { unwrapOrFromUndefinable } from '../unwrapOr.js';
+import { unwrapOrElseFromUndefinable } from '../unwrapOrElse.js';
+import { unwrapOrElseAsyncFromUndefinable } from '../unwrapOrElseAsync.js';
 
 /**
  *  @deprecated
- *  This is an alias for backward compatibility.
- *  Please use {@link expectNotUndefined} instead.
+ *  Please use {@link expectNotUndefined} in `option-t/Undefinable/Undefinable` or `option-t/Undefinable`.
  */
 export const expect: typeof expectNotUndefined = expectNotUndefined;
 
 /**
  *  @deprecated
- *  This is an alias for backward compatibility.
- *  Please use {@link unwrapUndefinable} instead.
+ *  Please use {@link unwrapUndefinable} in `option-t/Undefinable/unwrap` or `option-t/Undefinable`.
  */
 export const unwrap: typeof unwrapUndefinable = unwrapUndefinable;
+
+/**
+ *  @deprecated
+ *  Please use {@link andThenForUndefinable} in `option-t/Undefinable/andThen` or `option-t/Undefinable`.
+ */
+export const andThen: typeof andThenForUndefinable = andThenForUndefinable;
+
+/**
+ *  @deprecated
+ *  Please use {@link andThenAsyncForUndefinable} in `option-t/Undefinable/andThenAsync` or `option-t/Undefinable`.
+ */
+export const andThenAsync: typeof andThenAsyncForUndefinable = andThenAsyncForUndefinable;
+
+/**
+ *  @deprecated
+ *  Please use {@link inspectUndefinable} in `option-t/Undefinable/inspect` or `option-t/Undefinable`.
+ */
+export const inspect: typeof inspectUndefinable = inspectUndefinable;
+
+/**
+ *  @deprecated
+ *  Please use {@link mapForUndefinable} in `option-t/Undefinable/map` or `option-t/Undefinable`.
+ */
+export const map: typeof mapForUndefinable = mapForUndefinable;
+
+/**
+ *  @deprecated
+ *  Please use {@link mapAsyncForUndefinable} in `option-t/Undefinable/mapAsync` or `option-t/Undefinable`.
+ */
+export const mapAsync: typeof mapAsyncForUndefinable = mapAsyncForUndefinable;
+
+/**
+ *  @deprecated
+ *  Please use {@link mapOrForUndefinable} in `option-t/Undefinable/mapOr` or `option-t/Undefinable`.
+ */
+export const mapOr: typeof mapOrForUndefinable = mapOrForUndefinable;
+
+/**
+ *  @deprecated
+ *  Please use {@link mapOrAsyncForUndefinable} in `option-t/Undefinable/mapOrAsync` or `option-t/Undefinable`.
+ */
+export const mapOrAsync: typeof mapOrAsyncForUndefinable = mapOrAsyncForUndefinable;
+
+/**
+ *  @deprecated
+ *  Please use {@link mapOrElseForUndefinable} in `option-t/Undefinable/mapOrElse` or `option-t/Undefinable`.
+ */
+export const mapOrElse: typeof mapOrElseForUndefinable = mapOrElseForUndefinable;
+
+/**
+ *  @deprecated
+ *  Please use {@link mapOrElseAsyncForUndefinable} in `option-t/Undefinable/mapOrElseAsync` or `option-t/Undefinable`.
+ */
+export const mapOrElseAsync: typeof mapOrElseAsyncForUndefinable = mapOrElseAsyncForUndefinable;
+
+/**
+ *  @deprecated
+ *  Please use {@link orElseForUndefinable} in `option-t/Undefinable/orElse` or `option-t/Undefinable`.
+ */
+export const orElse: typeof orElseForUndefinable = orElseForUndefinable;
+
+/**
+ *  @deprecated
+ *  Please use {@link orElseAsyncForUndefinable} in `option-t/Undefinable/orElseAsync` or `option-t/Undefinable`.
+ */
+export const orElseAsync: typeof orElseAsyncForUndefinable = orElseAsyncForUndefinable;
+
+/**
+ *  @deprecated
+ *  Please use {@link unwrapOrFromUndefinable} in `option-t/Undefinable/unwrapOr` or `option-t/Undefinable`.
+ */
+export const unwrapOr: typeof unwrapOrFromUndefinable = unwrapOrFromUndefinable;
+
+/**
+ *  @deprecated
+ *  Please use {@link unwrapOrElseFromUndefinable} in `option-t/Undefinable/unwrapOrElse` or `option-t/Undefinable`.
+ */
+export const unwrapOrElse: typeof unwrapOrElseFromUndefinable = unwrapOrElseFromUndefinable;
+
+/**
+ *  @deprecated
+ *  Please use {@link unwrapOrElseAsyncFromUndefinable} in `option-t/Undefinable/unwrapOrElseAsync` or `option-t/Undefinable`.
+ */
+export const unwrapOrElseAsync: typeof unwrapOrElseAsyncFromUndefinable =
+    unwrapOrElseAsyncFromUndefinable;
