01a13fcd80cf405fab8d65902eb3d4f52646689e
Tetsuharu Ohzeki <tetsuharu.ohzeki@gmail.com>
2023-03-01 06:09:42 +0900

    Add explicit variance annotation for internals function types

--- packages/option-t/src/internal/Function.ts
+++ packages/option-t/src/internal/Function.ts
@@ -1,11 +1,11 @@
-export type TransformFn<T, U> = (input: T) => U;
-export type RecoveryFn<T> = () => T;
-export type RecoveryFromErrorFn<E, T> = TransformFn<E, T>;
-export type EffectFn<T> = (input: T) => void;
-export type FilterFn<T> = (input: T) => boolean;
-export type ProducerFn<T> = () => T;
+export type TransformFn<in T, out U> = (input: T) => U;
+export type RecoveryFn<out T> = () => T;
+export type RecoveryFromErrorFn<in E, out T> = TransformFn<E, T>;
+export type EffectFn<in T> = (input: T) => void;
+export type FilterFn<in T> = (input: T) => boolean;
+export type ProducerFn<out T> = () => T;
 
-export type AsyncTransformFn<T, U> = TransformFn<T, Promise<U>>;
-export type AsyncRecoveryFn<T> = RecoveryFn<Promise<T>>;
-export type AsyncRecoveryFromErrorFn<E, T> = AsyncTransformFn<E, T>;
-export type AsyncProducerFn<T> = ProducerFn<Promise<T>>;
+export type AsyncTransformFn<in T, out U> = TransformFn<T, Promise<U>>;
+export type AsyncRecoveryFn<out T> = RecoveryFn<Promise<T>>;
+export type AsyncRecoveryFromErrorFn<in E, out T> = AsyncTransformFn<E, T>;
+export type AsyncProducerFn<out T> = ProducerFn<Promise<T>>;
