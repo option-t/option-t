e6f89a3567bbc00b69e323ad6e1993cc70ef8eb2
Tetsuharu Ohzeki <tetsuharu.ohzeki@gmail.com>
2023-03-06 05:32:11 +0900

    Add `deprecated` jsdoc tag to `PlainOption/index` and `PlainOption/namespace`

--- packages/option-t/src/plain_option/namespace.ts
+++ packages/option-t/src/plain_option/namespace.ts
@@ -1,4 +1,10 @@
 /**
+ *  @deprecated
+ *  Consider to use `Nullable<T>`, `Undefinable<T>`, or `Maybe<T>` to express an absence of a value.
+ *  In JavaScript, they satisfy almost use cases. Probably, you might not have to use this type.
+ *
+ *  --------
+ *
  *  XXX:
  *  This module is designed to use as `import * as PlainOption from 'option-t/PlainOption/namespace';`.
  *  This is not designed for other usecases. Please do not use for other cases.
