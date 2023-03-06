e2c02fa45bf411d16c481c30d976b0883d83f543
Tetsuharu Ohzeki <tetsuharu.ohzeki@gmail.com>
2023-03-06 17:55:04 +0900

    Remove `assertIsPromise()` from `PlainResult/orElseAsync`

--- packages/option-t/src/plain_result/or_else_async.ts
+++ packages/option-t/src/plain_result/or_else_async.ts
@@ -1,5 +1,3 @@
-import { assertIsPromise } from '../internal/assert.js';
-import { ERR_MSG_RECOVERER_MUST_RETURN_PROMISE } from '../internal/error_message.js';
 import type { AsyncRecoveryFromErrorFn } from '../internal/function.js';
 import { type Result, isOk } from './result.js';
 import { unwrapErrFromResult } from './unwrap.js';
@@ -13,19 +11,15 @@ export type ResultAsyncTryRecoveryFromErrorFn<in E, out T, out F> = AsyncRecover
  *  Calls _recoverer_ and return its returned value if the result is `Err(E)`,
  *  otherwise returns the `Ok(T)` value of self.
  */
-export function orElseAsyncForResult<T, E, F>(
+export async function orElseAsyncForResult<T, E, F>(
     input: Result<T, E>,
     recoverer: ResultAsyncTryRecoveryFromErrorFn<E, T, F>
 ): Promise<Result<T, F>> {
     if (isOk(input)) {
-        return Promise.resolve(input);
+        return input;
     }
 
     const inner = unwrapErrFromResult(input);
-    const defaultValue: Promise<Result<T, F>> = recoverer(inner);
-    // If this is async function, this always return Promise, but not.
-    // We should check to clarify the error case if user call this function from plain js
-    // and they mistake to use this.
-    assertIsPromise(defaultValue, ERR_MSG_RECOVERER_MUST_RETURN_PROMISE);
+    const defaultValue: Result<T, F> = await recoverer(inner);
     return defaultValue;
 }
