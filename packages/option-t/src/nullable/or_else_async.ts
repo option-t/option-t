5e0381f73550701cc831aedc6885602cf53013ac
Tetsuharu Ohzeki <tetsuharu.ohzeki@gmail.com>
2023-03-18 17:32:22 +0900

    Remove `assertIsPromise()` from `Nullable/orElseAsync`

--- packages/option-t/src/nullable/or_else_async.ts
+++ packages/option-t/src/nullable/or_else_async.ts
@@ -1,5 +1,3 @@
-import { assertIsPromise } from '../internal/assert.js';
-import { ERR_MSG_RECOVERER_MUST_RETURN_PROMISE } from '../internal/error_message.js';
 import type { AsyncRecoveryFn } from '../internal/function.js';
 import { type Nullable, isNotNull } from './nullable.js';
 
@@ -9,18 +7,14 @@ export type NullableAsyncTryRecoveryFn<out T> = AsyncRecoveryFn<Nullable<T>>;
  *  Return _input_ as `T` if the passed _input_ is not `null`.
  *  Otherwise, return the result of _recoverer_.
  */
-export function orElseAsyncForNullable<T>(
+export async function orElseAsyncForNullable<T>(
     input: Nullable<T>,
     recoverer: NullableAsyncTryRecoveryFn<T>
 ): Promise<Nullable<T>> {
     if (isNotNull(input)) {
-        return Promise.resolve(input);
+        return input;
     }
 
-    const fallback: Promise<Nullable<T>> = recoverer();
-    // If this is async function, this always return Promise, but not.
-    // We should check to clarify the error case if user call this function from plain js
-    // and they mistake to use this.
-    assertIsPromise(fallback, ERR_MSG_RECOVERER_MUST_RETURN_PROMISE);
+    const fallback: Nullable<T> = await recoverer();
     return fallback;
 }
