083c36a36edd9b8d1443cd2ef620d2ad83823010
Tetsuharu Ohzeki <tetsuharu.ohzeki@gmail.com>
2023-03-18 16:36:17 +0900

    Remove `assertIsPromise()` from `PlainResult/tryCatchAsync`

--- packages/option-t/src/plain_result/try_catch_async.ts
+++ packages/option-t/src/plain_result/try_catch_async.ts
@@ -1,8 +1,5 @@
-import { assertIsErrorInstance, assertIsPromise } from '../internal/assert.js';
-import {
-    ERR_MSG_PRODUCER_MUST_RETURN_PROMISE,
-    ERR_MSG_THROWN_VALUE_IS_NOT_BUILTIN_ERROR_INSTANCE,
-} from '../internal/error_message.js';
+import { assertIsErrorInstance } from '../internal/assert.js';
+import { ERR_MSG_THROWN_VALUE_IS_NOT_BUILTIN_ERROR_INSTANCE } from '../internal/error_message.js';
 import type { AsyncProducerFn } from '../internal/function.js';
 import { mapErrForResult } from './map_err.js';
 import { type Result, createOk, createErr } from './result.js';
@@ -14,20 +11,18 @@ import { type Result, createOk, createErr } from './result.js';
  *  NOTE:
  *  An user should narrow the scope of _producer_ to make it predictable that is in `Err(E)`.
  */
-export function tryCatchIntoResultAsync<T>(
+export async function tryCatchIntoResultAsync<T>(
     producer: AsyncProducerFn<T>
 ): Promise<Result<T, unknown>> {
-    let value: Promise<T>;
+    let value: T;
     try {
-        value = producer();
+        value = await producer();
     } catch (e: unknown) {
         const errWrapped = createErr<unknown>(e);
-        return Promise.resolve(errWrapped);
+        return errWrapped;
     }
 
-    assertIsPromise(value, ERR_MSG_PRODUCER_MUST_RETURN_PROMISE);
-
-    const wrapped: Promise<Result<T, unknown>> = value.then(createOk, createErr);
+    const wrapped: Result<T, unknown> = createOk(value);
     return wrapped;
 }
 
