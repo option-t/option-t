0c37b7b59caf86ab54063fd2626fed50acf0211b
Tetsuharu Ohzeki <tetsuharu.ohzeki@gmail.com>
2023-03-18 16:30:45 +0900

    Remove `assertIsPromise()` from `PlainResult/mapOrAsync`

--- packages/option-t/src/plain_result/map_or_async.ts
+++ packages/option-t/src/plain_result/map_or_async.ts
@@ -1,5 +1,3 @@
-import { assertIsPromise } from '../internal/assert.js';
-import { ERR_MSG_TRANSFORMER_MUST_RETURN_PROMISE } from '../internal/error_message.js';
 import type { AsyncTransformFn } from '../internal/function.js';
 import { isErr, type Result } from './result.js';
 import { unwrapOkFromResult } from './unwrap.js';
@@ -10,21 +8,16 @@ import { unwrapOkFromResult } from './unwrap.js';
  *
  *  Basically, this operation is a combination `mapAsync()` and `unwrapOr()`.
  */
-export function mapOrAsyncForResult<T, E, U>(
+export async function mapOrAsyncForResult<T, E, U>(
     input: Result<T, E>,
     defaultValue: U,
     transformer: AsyncTransformFn<T, U>
 ): Promise<U> {
     if (isErr(input)) {
-        const result: Promise<U> = Promise.resolve(defaultValue);
-        return result;
+        return defaultValue;
     }
 
     const source: T = unwrapOkFromResult(input);
-    const result: Promise<U> = transformer(source);
-    // If this is async function, this always return Promise, but not.
-    // We should check to clarify the error case if user call this function from plain js
-    // and they mistake to use this.
-    assertIsPromise(result, ERR_MSG_TRANSFORMER_MUST_RETURN_PROMISE);
+    const result: U = await transformer(source);
     return result;
 }
