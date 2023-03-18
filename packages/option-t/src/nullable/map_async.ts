110a9f82ae178fdce4abafd790b271a1f1aa101e
Tetsuharu Ohzeki <tetsuharu.ohzeki@gmail.com>
2023-03-18 17:28:34 +0900

    Remove `assertIsPromise()` from `Nullable/mapAsync`

--- packages/option-t/src/nullable/map_async.ts
+++ packages/option-t/src/nullable/map_async.ts
@@ -2,13 +2,6 @@ import type { AsyncTransformFn } from '../internal/function.js';
 import { type Nullable, isNull, type NotNull } from './nullable.js';
 import { ERR_MSG_TRANSFORMER_MUST_NOT_RETURN_NO_VAL_FOR_NULLABLE } from './internal/error_message.js';
 import { expectNotNull } from './expect.js';
-import { assertIsPromise } from '../internal/assert.js';
-import { ERR_MSG_TRANSFORMER_MUST_RETURN_PROMISE } from '../internal/error_message.js';
-
-function check<T>(value: Nullable<T>): T {
-    const passed = expectNotNull(value, ERR_MSG_TRANSFORMER_MUST_NOT_RETURN_NO_VAL_FOR_NULLABLE);
-    return passed;
-}
 
 /**
  *  Return the result of _transformer_ with using _input_ as an argument for it if _input_ is not `null`,
@@ -18,25 +11,23 @@ function check<T>(value: Nullable<T>): T {
  *      * If you'd like return `Nullable<*>` as `U`, use `andThen()`.
  *      * If the result of _transformer_ is `null`, this throw an `Error`.
  */
-export function mapAsyncForNullable<T, U>(
+export async function mapAsyncForNullable<T, U>(
     input: Nullable<T>,
     transformer: AsyncTransformFn<T, NotNull<U>>
 ): Promise<Nullable<U>> {
     if (isNull(input)) {
-        return Promise.resolve(input);
+        return null;
     }
 
-    const result = transformer(input);
-    // If this is async function, this always return Promise, but not.
-    // We should check to clarify the error case if user call this function from plain js
-    // and they mistake to use this.
-    assertIsPromise(result, ERR_MSG_TRANSFORMER_MUST_RETURN_PROMISE);
-
+    const result: NotNull<U> = await transformer(input);
     // XXX:
     // If `U` is `Nullable<SomeType>`, we think naturally the returned value of this function would be
     // the nested type `Nullable<Nullable<SomeType>>`. But this type means `(SomeType | null) | null`.
     // So a type checker would recognize this type as `SomeType | null`. So it's flattened.
     // Then the user should call `andThen` (_flatmap_) operation instead of this.
-    const passed = result.then(check);
+    const passed: NotNull<U> = expectNotNull(
+        result,
+        ERR_MSG_TRANSFORMER_MUST_NOT_RETURN_NO_VAL_FOR_NULLABLE
+    );
     return passed;
 }
