80bced3385b55ad7f73f5070158b7caa642b3767
Tetsuharu Ohzeki <tetsuharu.ohzeki@gmail.com>
2023-03-19 02:50:00 +0900

    Remove `assertIsPromise()` from `PlainOption/unwrapOrElseAsync`

--- packages/option-t/src/plain_option/unwrap_or_else_async.ts
+++ packages/option-t/src/plain_option/unwrap_or_else_async.ts
@@ -1,5 +1,3 @@
-import { assertIsPromise } from '../internal/assert.js';
-import { ERR_MSG_RECOVERER_MUST_RETURN_PROMISE } from '../internal/error_message.js';
 import type { AsyncRecoveryFn } from '../internal/function.js';
 import { type Option, isSome } from './option.js';
 import { unwrapOption } from './unwrap.js';
@@ -8,19 +6,15 @@ import { unwrapOption } from './unwrap.js';
  *  Unwraps a result _input_, returns the content of an `Some(T)`.
  *  If the value is an `None` then it calls `def` with its value.
  */
-export function unwrapOrElseAsyncFromOption<T>(
+export async function unwrapOrElseAsyncFromOption<T>(
     input: Option<T>,
     recoverer: AsyncRecoveryFn<T>
 ): Promise<T> {
     if (isSome(input)) {
         const val: T = unwrapOption(input);
-        return Promise.resolve(val);
+        return val;
     }
 
-    const defaultValue: Promise<T> = recoverer();
-    // If this is async function, this always return Promise, but not.
-    // We should check to clarify the error case if user call this function from plain js
-    // and they mistake to use this.
-    assertIsPromise(defaultValue, ERR_MSG_RECOVERER_MUST_RETURN_PROMISE);
+    const defaultValue: T = await recoverer();
     return defaultValue;
 }
