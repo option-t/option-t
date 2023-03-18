fe6028ae8e288225e258766aff87b183a07e5192
Tetsuharu Ohzeki <tetsuharu.ohzeki@gmail.com>
2023-03-19 02:47:35 +0900

    Remove `assertIsPromise()` from `PlainOption/mapOrElseAsync`

--- packages/option-t/src/plain_option/map_or_else_async.ts
+++ packages/option-t/src/plain_option/map_or_else_async.ts
@@ -1,8 +1,3 @@
-import { assertIsPromise } from '../internal/assert.js';
-import {
-    ERR_MSG_RECOVERER_MUST_RETURN_PROMISE,
-    ERR_MSG_TRANSFORMER_MUST_RETURN_PROMISE,
-} from '../internal/error_message.js';
 import type { AsyncTransformFn, AsyncRecoveryFn } from '../internal/function.js';
 import { type Option, isNone } from './option.js';
 import { unwrapOption } from './unwrap.js';
@@ -12,25 +7,17 @@ import { unwrapOption } from './unwrap.js';
  *  or a _recoverer_ function to a contained `None` value in _input_.
  *  This function can be used to unpack a successful result while handling an error.
  */
-export function mapOrElseAsyncForOption<T, U>(
+export async function mapOrElseAsyncForOption<T, U>(
     input: Option<T>,
     recoverer: AsyncRecoveryFn<U>,
     transformer: AsyncTransformFn<T, U>
 ): Promise<U> {
     if (isNone(input)) {
-        const fallback: Promise<U> = recoverer();
-        // If this is async function, this always return Promise, but not.
-        // We should check to clarify the error case if user call this function from plain js
-        // and they mistake to use this.
-        assertIsPromise(fallback, ERR_MSG_RECOVERER_MUST_RETURN_PROMISE);
+        const fallback: U = await recoverer();
         return fallback;
     }
 
     const inner: T = unwrapOption(input);
-    const result: Promise<U> = transformer(inner);
-    // If this is async function, this always return Promise, but not.
-    // We should check to clarify the error case if user call this function from plain js
-    // and they mistake to use this.
-    assertIsPromise(result, ERR_MSG_TRANSFORMER_MUST_RETURN_PROMISE);
+    const result: U = await transformer(inner);
     return result;
 }
