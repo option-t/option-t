adde4f50ff31fb6a73a43090a6c071950e552af8
Tetsuharu Ohzeki <tetsuharu.ohzeki@gmail.com>
2023-03-18 18:49:19 +0900

    Remove `assertIsPromise()` from `Undefinable/andThenAsync`

--- packages/option-t/src/undefinable/and_then_async.ts
+++ packages/option-t/src/undefinable/and_then_async.ts
@@ -1,5 +1,3 @@
-import { assertIsPromise } from '../internal/assert.js';
-import { ERR_MSG_TRANSFORMER_MUST_RETURN_PROMISE } from '../internal/error_message.js';
 import type { AsyncTransformFn } from '../internal/function.js';
 
 import { type Undefinable, isUndefined } from './undefinable.js';
@@ -15,18 +13,14 @@ export type UndefinableAsyncTryTransformFn<in T, out U> = AsyncTransformFn<T, Un
  *  But we don't provide `flatMap()` as alias of this function.
  *  because it's too hard to undarstand that "flatMap" operation for `T | undefined`
  */
-export function andThenAsyncForUndefinable<T, U>(
+export async function andThenAsyncForUndefinable<T, U>(
     input: Undefinable<T>,
     transformer: UndefinableAsyncTryTransformFn<T, U>
 ): Promise<Undefinable<U>> {
     if (isUndefined(input)) {
-        return Promise.resolve<Undefinable<U>>(input);
+        return undefined;
     }
 
-    const result: Promise<Undefinable<U>> = transformer(input);
-    // If this is async function, this always return Promise, but not.
-    // We should check to clarify the error case if user call this function from plain js
-    // and they mistake to use this.
-    assertIsPromise(result, ERR_MSG_TRANSFORMER_MUST_RETURN_PROMISE);
+    const result: Undefinable<U> = await transformer(input);
     return result;
 }
