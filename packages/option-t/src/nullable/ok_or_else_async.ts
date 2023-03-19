6e2d461e318d0f3993b8933d4c6f9d78bc6a89b7
Tetsuharu Ohzeki <tetsuharu.ohzeki@gmail.com>
2023-03-20 00:14:31 +0900

    Remove `assertIsPromise()` from `Nullable/okOrElseAsync`

--- packages/option-t/src/nullable/ok_or_else_async.ts
+++ packages/option-t/src/nullable/ok_or_else_async.ts
@@ -1,5 +1,3 @@
-import { assertIsPromise } from '../internal/assert.js';
-import { ERR_MSG_RECOVERER_MUST_RETURN_PROMISE } from '../internal/error_message.js';
 import type { AsyncRecoveryFn } from '../internal/function.js';
 
 import { type Nullable, isNotNull } from './nullable.js';
@@ -9,20 +7,16 @@ import { type Result, createErr, createOk } from '../plain_result/result.js';
  * Transforms the `Nullable<T>` into a `Result<T, E>` by mapping `T` to `Ok(T)`.
  * If `input` is `null`, then returns `Err(E)` with the result of `recoverer()`
  */
-export function okOrElseAsyncForNullable<T, E>(
+export async function okOrElseAsyncForNullable<T, E>(
     input: Nullable<T>,
     recoverer: AsyncRecoveryFn<E>
 ): Promise<Result<T, E>> {
     if (isNotNull(input)) {
         const okWrapped = createOk<T>(input);
-        return Promise.resolve(okWrapped);
+        return okWrapped;
     }
 
-    const fallback: Promise<E> = recoverer();
-    // If this is async function, this always return Promise, but not.
-    // We should check to clarify the error case if user call this function from plain js
-    // and they mistake to use this.
-    assertIsPromise(fallback, ERR_MSG_RECOVERER_MUST_RETURN_PROMISE);
-    const errWrapped = fallback.then(createErr);
+    const fallback: E = await recoverer();
+    const errWrapped = createErr<E>(fallback);
     return errWrapped;
 }
