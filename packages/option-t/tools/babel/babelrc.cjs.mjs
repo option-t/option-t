07180809bc21bfb38c4f78a1c743fd3a27553a85
Tetsuharu Ohzeki <tetsuharu.ohzeki@gmail.com>
2023-03-25 19:48:52 +0900

    Simplify the build process
    By this change, input files must be TypeScript.

--- packages/option-t/tools/babel/babelrc.cjs.mjs
+++ packages/option-t/tools/babel/babelrc.cjs.mjs
@@ -1,22 +1,11 @@
 import * as path from 'node:path';
 import { fileURLToPath } from 'node:url';
 
-import { babelEnvPresetConfig } from './babelconfig.mjs';
-
 const THIS_FILENAME = fileURLToPath(import.meta.url);
 const THIS_DIRNAME = path.dirname(THIS_FILENAME);
 const pathResolve = path.resolve.bind(undefined, THIS_DIRNAME);
 
 export default {
-    presets: [
-        [
-            '@babel/preset-env',
-            {
-                ...babelEnvPresetConfig,
-                modules: false,
-            },
-        ],
-    ],
     plugins: [
         [
             pathResolve('./babel-plugin-modify-ext.mjs'),
