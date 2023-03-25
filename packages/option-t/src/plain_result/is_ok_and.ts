1672b9ab1c92ee689458c0680b27d3054ed2ceb8
Tetsuharu Ohzeki <tetsuharu.ohzeki@gmail.com>
2023-03-26 02:41:14 +0900

    fixup! wip

--- packages/option-t/src/plain_result/is_ok_and.ts
+++ packages/option-t/src/plain_result/is_ok_and.ts
@@ -4,6 +4,9 @@ import { type Result, type Ok, isErr, unwrapOk } from './result.js';
 /**
  * Returns `true` if the _result_ is `Ok<T>` and the value inside of it matches a _predicate_.
  */
+// XXX:
+// We cannot use `result is Ok<T>` as the returned type here because of that
+// it **does not mean** "result is not Ok<T>" even if _predicate_ returns `false`.
 export function isOkAnd<T, E>(result: Result<T, E>, predicate: FilterFn<T>): boolean {
     if (isErr(result)) {
         return false;
