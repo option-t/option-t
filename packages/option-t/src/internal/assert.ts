ae14bfaef3b2dd1ec1f71576e89622fb3048373c
Tetsuharu Ohzeki <tetsuharu.ohzeki@gmail.com>
2023-04-06 19:16:29 +0900

    Add the fallback path for `String(input)` failure in `assertIsErrorInstance()`

--- packages/option-t/src/internal/assert.ts
+++ packages/option-t/src/internal/assert.ts
@@ -1,15 +1,46 @@
 import { ERR_MSG_INPUT_IS_FROZEN_NOT_CAST_TO_MUTABLE } from './error_message.js';
 
-export function assertIsErrorInstance(input: unknown, message: string): asserts input is Error {
-    const ok = input instanceof Error;
-    if (!ok) {
-        const stringified = String(input);
-        const msg = `${message} The actual is \`${stringified}\``;
-        // We don't throw Node's AssertionError because the code size will be larger.
-        throw new TypeError(msg, {
-            cause: input,
+interface StringifyResult {
+    cause: unknown;
+    text: string;
+}
+
+function tryToString(input: unknown, message: string): StringifyResult {
+    // We would not like to use `Nullable<T>` here to avoid complex module dependency cycle.
+    let caught: Error | null = null;
+    let stringified = '';
+
+    try {
+        stringified = String(input);
+    } catch (e) {
+        caught = new TypeError(`fail toString()`, {
+            cause: e,
         });
     }
+
+    const isSuccessStringify = !caught;
+    const cause = isSuccessStringify ? input : caught;
+    const text = isSuccessStringify ? `${message} The actual is \`${stringified}\`` : message;
+
+    return {
+        cause,
+        text,
+    };
+}
+
+export function assertIsErrorInstance(input: unknown, message: string): asserts input is Error {
+    const ok = input instanceof Error;
+    if (ok) {
+        return;
+    }
+
+    // FIXME: https://github.com/option-t/option-t/issues/1832
+    // We should only use `Error.prototype.cause`.
+    const { cause, text } = tryToString(input, message);
+    // We don't throw Node's AssertionError because the code size will be larger.
+    throw new TypeError(text, {
+        cause,
+    });
 }
 
 export function assertIsFrozen(input: unknown): void {
