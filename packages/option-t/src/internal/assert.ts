fc83496a090d6f10d78361e6736d5d40a9c2faae
Tetsuharu Ohzeki <tetsuharu.ohzeki@gmail.com>
2023-04-06 19:35:38 +0900

    Set `Error.cause` in `assertIsErrorInstance()` by hand if it is not implemented by the host environment
    I prefer this solution than #1831.
    Fix #1832
    
    Motivation
    ---------------
    
    Why we call `String(input)` in `assertIsErrorInstance()`
    https://github.com/option-t/option-t/blame/18b119a2bab1bda6c68e845a544efa523d243994/src/internal/assert.ts#L15-L16
    
    is to improve debuggability on devtools.
    But I slacked to implement it with some compat path with `Error.cause`.
    As the result, we need to create a patch as #1831.
    
    This patch rewrite the path for non `Error` instance in `assertIsErrorInstance()`
    with implementing `Error.cause` polyfill (Of cource we would like to remove it in the future).
    
    This simplify the implementation and take more standard like way.
    
    Drawbacks
    ----------
    
    Performance
    ============
    
    This path is a fallback path for the place which we expect coming an `Error` instance.
    I think we can allow some slow down.
    
    Changing an error message format
    =============
    
    This is a pain point but I think this would not be marked
    as a breaking change because the almost behavior is not changed.
    However, this would be a problem if user's observability tool
    does not support `Error.cause`. But it's a another problem, I think.

--- packages/option-t/src/internal/assert.ts
+++ packages/option-t/src/internal/assert.ts
@@ -1,45 +1,32 @@
 import { ERR_MSG_INPUT_IS_FROZEN_NOT_CAST_TO_MUTABLE } from './error_message.js';
 
-interface StringifyResult {
-    cause: unknown;
-    text: string;
-}
-
-function tryToString(input: unknown, message: string): StringifyResult {
-    // We would not like to use `Nullable<T>` here to avoid complex module dependency cycle.
-    let caught: Error | null = null;
-    let stringified = '';
-
-    try {
-        stringified = String(input);
-    } catch (e) {
-        caught = new TypeError(`fail toString()`, {
-            cause: e,
-        });
-    }
-
-    const isSuccessStringify = !caught;
-    const cause = isSuccessStringify ? input : caught;
-    const text = isSuccessStringify ? `${message} The actual is \`${stringified}\`` : message;
-
-    return {
-        cause,
-        text,
-    };
-}
-
 export function assertIsErrorInstance(input: unknown, message: string): asserts input is Error {
     const ok = input instanceof Error;
     if (ok) {
         return;
     }
 
-    // FIXME: https://github.com/option-t/option-t/issues/1832
-    // We should only use `Error.prototype.cause`.
-    const { cause, text } = tryToString(input, message);
     // We don't throw Node's AssertionError because the code size will be larger.
-    throw new TypeError(text, {
-        cause,
+    const e = new TypeError(message, {
+        cause: input,
+    });
+
+    // Check each time to avoid the timing problem to install the polyfill.
+    // FIXME(#1833): We should remove this path.
+    if (e.cause !== input && Object.isExtensible(e)) {
+        installErrorCauseAsPolyfill(e, input);
+    }
+
+    throw e;
+}
+
+function installErrorCauseAsPolyfill(e: TypeError, input: unknown) {
+    // see https://tc39.es/ecma262/multipage/fundamental-objects.html#sec-installerrorcause
+    Object.defineProperty(e, 'cause', {
+        value: input,
+        writable: true,
+        enumerable: false,
+        configurable: true,
     });
 }
 
