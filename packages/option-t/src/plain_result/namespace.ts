e8deae2ec2890b4130314f1425abf1aa24327939
Tetsuharu Ohzeki <tetsuharu.ohzeki@gmail.com>
2024-03-29 23:46:10 +0900

    Export more items from `PlainResult/namespace`

--- packages/option-t/src/plain_result/namespace.ts
+++ packages/option-t/src/plain_result/namespace.ts
@@ -20,6 +20,7 @@ export {
 export { andThenForResult as andThen } from './and_then.js';
 export { andThenAsyncForResult as andThenAsync } from './and_then_async.js';
 export { flattenForResult as flatten } from './flatten.js';
+export { fromPromiseSettledResultToResult as fromPromiseSettledResult } from './from_promise_settled_result.js';
 export {
     inspectBothOfResult as inspectBoth,
     inspectErrOfResult as inspectErr,
@@ -36,11 +37,20 @@ export { mapOrElseAsyncForResult as mapOrElseAsync } from './map_or_else_async.j
 export { orElseForResult as orElse } from './or_else.js';
 export { orElseAsyncForResult as orElseAsync } from './or_else_async.js';
 export { toNullableFromErr, toNullableFromOk } from './to_nullable.js';
+// We don't export to_option since its type has been deprecated. #2062
 export { toUndefinableFromErr, toUndefinableFromOk } from './to_undefinable.js';
 export {
     transposeNullableForResult as transposeNullable,
     transposeUndefinableForResult as transposeUndefinable,
 } from './transpose.js';
+export {
+    tryCatchIntoResult as tryCatchInto,
+    tryCatchIntoResultWithEnsureError as tryCatchIntoWithEnsureError,
+} from './try_catch.js';
+export {
+    tryCatchIntoResultAsync as tryCatchIntoAsync,
+    tryCatchIntoResultWithEnsureErrorAsync as tryCatchIntoWithEnsureErrorAsync,
+} from './try_catch_async.js';
 export { unwrapOrFromResult as unwrapOr } from './unwrap_or.js';
 export { unwrapOrElseFromResult as unwrapOrElse } from './unwrap_or_else.js';
 export { unwrapOrElseAsyncFromResult as unwrapOrElseAsync } from './unwrap_or_else_async.js';
