878f3c7245c04810a145cdf87ca892f00245dc36
Tetsuharu Ohzeki <tetsuharu.ohzeki@gmail.com>
2024-04-03 07:46:24 +0900

    Annotate filterWithEnsureType more correctly

--- packages/option-t/src/nullable/filter.ts
+++ packages/option-t/src/nullable/filter.ts
@@ -1,5 +1,5 @@
 import type { FilterFn, TypePredicateFn } from '../internal/function.js';
-import { type Nullable, isNotNull } from './nullable.js';
+import { type Nullable, isNotNull, type NotNull } from './nullable.js';
 
 /**
  *  Returns `null` if the _input_ is `null`,
@@ -23,18 +23,18 @@ export function filterForNullable<T>(input: Nullable<T>, predicate: FilterFn<T>)
  *  Returns `null` if the _input_ is `null`,
  *  otherwise calls _predicate_ with the _input_ `T` and returns:
  *
- *      * `I` if _predicate_ returns `true` which means that _predicate_ says the input value is U.
+ *      * The input value as `U` if _predicate_ returns `true` which means that _predicate_ says the input value is U.
  *      * `null` if _predicate_ returns `false`.
  *
  *  Please use {@link filterForNullable} generally if you don't have to narrow the type.
  */
 export function filterWithEnsureTypeForNullable<T, U extends T>(
     input: Nullable<T>,
-    predicate: TypePredicateFn<T, U>,
+    predicate: TypePredicateFn<T, NotNull<U>>,
 ): Nullable<U> {
     if (isNotNull(input)) {
         if (predicate(input)) {
-            const result: Nullable<U> = input;
+            const result: NotNull<U> = input;
             return result;
         }
     }
