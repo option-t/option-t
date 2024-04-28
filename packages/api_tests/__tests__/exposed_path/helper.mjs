5d811e94f92e7dfd80f4f0ebd8c0c710b00773b9
Tetsuharu Ohzeki <tetsuharu.ohzeki@gmail.com>
2024-04-29 03:22:25 +0900

    Remove `option-t/{cjs,esm}/` specific test case from unit tests

--- packages/api_tests/__tests__/exposed_path/helper.mjs
+++ packages/api_tests/__tests__/exposed_path/helper.mjs
@@ -2,30 +2,12 @@ import * as assert from 'node:assert/strict';
 import { createRequire } from 'node:module';
 const require = createRequire(import.meta.url);
 
-const ImportType = Object.freeze({
-    Both: 'both',
-    CJS: 'cjs',
-    ESM: 'esm',
-});
-
 const PACKAGE_NAME = 'option-t';
 
 function pathToNormalizedImport(pathname) {
-    let type;
-    if (pathname === '.') {
-        type = ImportType.Both;
-    } else if (pathname.startsWith('./cjs/')) {
-        type = ImportType.CJS;
-    } else if (pathname.startsWith('./esm/')) {
-        type = ImportType.ESM;
-    } else {
-        type = ImportType.Both;
-    }
-
     const input = pathname.replace(/^./u, PACKAGE_NAME);
 
     return {
-        type,
         raw: pathname,
         input,
     };
@@ -39,46 +21,26 @@ export function testImportExposedPath(test, pathList) {
     assert.ok(pathList.length > 0);
 
     const testcaseList = pathList.map(pathToNormalizedImport);
-    for (const { type, input } of testcaseList) {
+    for (const { input } of testcaseList) {
         test(input, async (t) => {
             let actual = null;
-            switch (type) {
-                case ImportType.Both: {
-                    t.plan(4);
-                    let esmActual;
-                    await t.notThrowsAsync(async () => {
-                        esmActual = await import(input);
-                    }, MSG_FAIL_TO_IMPORT_ESM);
+            {
+                t.plan(4);
+                let esmActual;
+                await t.notThrowsAsync(async () => {
+                    esmActual = await import(input);
+                }, MSG_FAIL_TO_IMPORT_ESM);
 
-                    let cjsActual;
-                    t.notThrows(() => {
-                        cjsActual = require(input);
-                    }, MSG_FAIL_TO_IMPORT_CJS);
-                    t.deepEqual(
-                        Object.keys(esmActual),
-                        Object.keys(cjsActual),
-                        'esm and cjs should has same named items',
-                    );
-                    actual = esmActual;
-                    break;
-                }
-                case ImportType.ESM: {
-                    t.plan(2);
-                    await t.notThrowsAsync(async () => {
-                        actual = await import(input);
-                    }, MSG_FAIL_TO_IMPORT_ESM);
-                    break;
-                }
-                case ImportType.CJS: {
-                    t.plan(2);
-                    t.notThrows(() => {
-                        actual = require(input);
-                    }, MSG_FAIL_TO_IMPORT_CJS);
-                    break;
-                }
-                default:
-                    t.fail(`with unknown type: ${type}`);
-                    return;
+                let cjsActual;
+                t.notThrows(() => {
+                    cjsActual = require(input);
+                }, MSG_FAIL_TO_IMPORT_CJS);
+                t.deepEqual(
+                    Object.keys(esmActual),
+                    Object.keys(cjsActual),
+                    'esm and cjs should has same named items',
+                );
+                actual = esmActual;
             }
 
             t.snapshot(Object.keys(actual), 'should match expected exported items');
