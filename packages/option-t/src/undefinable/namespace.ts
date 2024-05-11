07bacff5a5c455a89b511435c1667270193f3acd
Tetsuharu Ohzeki <tetsuharu.ohzeki@gmail.com>
2024-05-12 02:56:04 +0900

    Remove unnecessary `@experimental` JSDoc tags

--- packages/option-t/src/undefinable/namespace.ts
+++ packages/option-t/src/undefinable/namespace.ts
@@ -1,6 +1,1 @@
-/**
- *  @experimental
- *  This path is still experimental and unstable.
- *  We might change this path and exposed items from here without breaking change.
- */
 export * as Undefinable from './internal/intermediate_namespace.js';
