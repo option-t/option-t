448cc3037dd0e582eda56c4b2bc470edf3001031
Tetsuharu Ohzeki <tetsuharu.ohzeki@gmail.com>
2024-05-12 02:51:57 +0900

    Export `<TypeName>` namespace module object only from `<TypeName>/namespace`.

---
+++ packages/option-t/src/plain_option/ns.ts
@@ -0,0 +1,7 @@
+/**
+ *  @deprecated 37.1.0
+ *
+ *  Consider to use `Nullable<T>`, `Undefinable<T>`, or `Maybe<T>` to express an absence of a value.
+ *  In JavaScript, they satisfy almost use cases. Probably, you might not have to use this type.
+ */
+export * as Option from './internal/intermediate_namespace.js';
