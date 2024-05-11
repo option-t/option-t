01f501af767248ec26452bf222bc0022367744bc
Tetsuharu Ohzeki <tetsuharu.ohzeki@gmail.com>
2024-05-12 01:17:36 +0900

    `./Nullable/ns` should export items as `Nullable`

--- packages/option-t/src/nullable/ns.ts
+++ packages/option-t/src/nullable/ns.ts
@@ -3,4 +3,4 @@
  *  This path is still experimental and unstable.
  *  We might change this path and exposed items from here without breaking change.
  */
-export * as Result from './internal/intermediate_namespace.js';
+export * as Nullable from './internal/intermediate_namespace.js';
