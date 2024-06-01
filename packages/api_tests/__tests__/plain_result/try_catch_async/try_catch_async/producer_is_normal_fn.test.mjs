0ecbc9d3408fb0dcbc1daf2074ad82508d79034a
Tetsuharu Ohzeki <tetsuharu.ohzeki@gmail.com>
2024-06-02 02:43:14 +0900

    Split testcases for plain_result/try_catch into normal fn & async fn

--- packages/api_tests/__tests__/plain_result/try_catch_async/try_catch_async/producer_is_normal_fn.test.mjs
+++ packages/api_tests/__tests__/plain_result/try_catch_async/try_catch_async/producer_is_normal_fn.test.mjs
@@ -8,22 +8,6 @@ import {
 } from 'option-t/plain_result/result';
 import { tryCatchIntoResultAsync } from 'option-t/plain_result/try_catch_async';
 
-test('output=Ok(T): producer is async fn', async (t) => {
-    t.plan(4);
-
-    const EXPECTED = Math.random();
-    const result = tryCatchIntoResultAsync(async () => {
-        t.pass();
-        return EXPECTED;
-    });
-
-    t.true(result instanceof Promise, 'result should be Promise');
-
-    const actual = await result;
-    t.true(isOk(actual), 'should be Ok(T)');
-    t.is(unwrapOkFromResult(actual), EXPECTED, 'should contain the expect inner value');
-});
-
 test('output=Ok(T): producer is normal fn', async (t) => {
     t.plan(4);
 
@@ -38,22 +22,6 @@ test('output=Ok(T): producer is normal fn', async (t) => {
     const actual = await result;
     t.true(isOk(actual), 'should be Ok(T)');
     t.is(unwrapOkFromResult(actual), EXPECTED, 'should contain the expect inner value');
-});
-
-test('output=Err(unknown): producer is async fn', async (t) => {
-    t.plan(4);
-
-    const EXPECTED = new Error(Math.random());
-    const result = tryCatchIntoResultAsync(async () => {
-        t.pass();
-        throw EXPECTED;
-    });
-
-    t.true(result instanceof Promise, 'result should be Promise');
-
-    const actual = await result;
-    t.true(isErr(actual), 'should be Err(E)');
-    t.is(unwrapErrFromResult(actual), EXPECTED, 'should contain the expect inner value');
 });
 
 test('output=Err(unknown): producer is normal fn', async (t) => {
--- packages/api_tests/__tests__/plain_result/try_catch_async/try_catch_with_ensure_error_async/producer_is_normal_fn.test.mjs
+++ packages/api_tests/__tests__/plain_result/try_catch_async/try_catch_with_ensure_error_async/producer_is_normal_fn.test.mjs
@@ -8,22 +8,6 @@ import {
 } from 'option-t/plain_result/result';
 import { tryCatchIntoResultWithEnsureErrorAsync } from 'option-t/plain_result/try_catch_async';
 
-test('output=Ok(T): producer is async fn', async (t) => {
-    t.plan(4);
-
-    const EXPECTED = Math.random();
-    const result = tryCatchIntoResultWithEnsureErrorAsync(async () => {
-        t.pass();
-        return EXPECTED;
-    });
-
-    t.true(result instanceof Promise, 'result should be Promise');
-
-    const actual = await result;
-    t.true(isOk(actual), 'should be Ok(T)');
-    t.is(unwrapOkFromResult(actual), EXPECTED, 'should contain the expect inner value');
-});
-
 test('output=Ok(T): producer is normal fn', async (t) => {
     t.plan(4);
 
@@ -38,22 +22,6 @@ test('output=Ok(T): producer is normal fn', async (t) => {
     const actual = await result;
     t.true(isOk(actual), 'should be Ok(T)');
     t.is(unwrapOkFromResult(actual), EXPECTED, 'should contain the expect inner value');
-});
-
-test('output=Err(Error): producer is async fn', async (t) => {
-    t.plan(4);
-
-    const EXPECTED = new Error(Math.random());
-    const result = tryCatchIntoResultWithEnsureErrorAsync(async () => {
-        t.pass();
-        throw EXPECTED;
-    });
-
-    t.true(result instanceof Promise, 'result should be Promise');
-
-    const actual = await result;
-    t.true(isErr(actual), 'should be Err(E)');
-    t.is(unwrapErrFromResult(actual), EXPECTED, 'should contain the expect inner value');
 });
 
 test('output=Err(Error): producer is normal fn', async (t) => {
@@ -119,28 +87,6 @@ test('if producer is normal function and throw a not-Error-instance value before
     const actual = await t.throwsAsync(
         async () => {
             await tryCatchIntoResultWithEnsureErrorAsync(() => {
-                t.pass('producer is called');
-                throw THROWN_EXPECTED;
-            });
-            t.fail('unreachable here');
-        },
-        {
-            instanceOf: TypeError,
-            message: `The thrown value is not an \`Error\` instance.`,
-        },
-    );
-
-    t.is(actual.cause, THROWN_EXPECTED, 'should set Error.cause');
-});
-
-test('if producer is async function and throw a not-Error-instance value', async (t) => {
-    t.plan(3);
-
-    const THROWN_EXPECTED = Math.random();
-
-    const actual = await t.throwsAsync(
-        async () => {
-            await tryCatchIntoResultWithEnsureErrorAsync(async () => {
                 t.pass('producer is called');
                 throw THROWN_EXPECTED;
             });
