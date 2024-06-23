054dc69f6dbe29ff2ffecab86c18aa80c10f7a37
Tetsuharu Ohzeki <tetsuharu.ohzeki@gmail.com>
2024-06-23 10:36:58 +0900

    Enable TypeScript's `--isolatedDeclarations`

--- packages/option-t/src/internal/error_message.ts
+++ packages/option-t/src/internal/error_message.ts
@@ -4,11 +4,15 @@ const TRANSFORM_FUNCTION_NAME = '`transformer`';
 const RECOVERY_FUNCTION_NAME = '`recoverer`';
 const DEFAULT_VALUE_NAME = '`defaultValue`';
 
-export const ERR_MSG_TRANSFORMER_MUST_NOT_RETURN =
+export const ERR_MSG_TRANSFORMER_MUST_NOT_RETURN: string =
     TRANSFORM_FUNCTION_NAME + ERR_MSG_MUST_NOT_RETURN;
+
 export const ERR_MSG_CALLED_WITH = 'called with ';
-export const ERR_MSG_DEFAULT_VALUE_MUST_NOT_BE = DEFAULT_VALUE_NAME + ' must not be ';
-export const ERR_MSG_RECOVERER_MUST_NOT_RETURN = RECOVERY_FUNCTION_NAME + ERR_MSG_MUST_NOT_RETURN;
+
+export const ERR_MSG_DEFAULT_VALUE_MUST_NOT_BE: string = DEFAULT_VALUE_NAME + ' must not be ';
+
+export const ERR_MSG_RECOVERER_MUST_NOT_RETURN: string =
+    RECOVERY_FUNCTION_NAME + ERR_MSG_MUST_NOT_RETURN;
 
 export const ERR_MSG_INPUT_IS_FROZEN_NOT_CAST_TO_MUTABLE =
     'input is frozen, cannot cast to mutable';
