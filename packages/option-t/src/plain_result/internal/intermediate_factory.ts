b909f1a2167d385ef96450f040c8d1498b2cf873
Tetsuharu Ohzeki <tetsuharu.ohzeki@gmail.com>
2025-09-11 18:59:09 +0900

    Revert 8339e5f316702639b3ce2c487ad974969717f427
    After some thought, I concluded it makes a thing complex.
    
    The original motivation is just to achive both of followings:
    
    1. Bundle multiple operator functions into the single namespace object.
    2. But we'd like to avoid a redundancy as like as `Result.Result`.
    
    `ResultOperator` might not be nice naming to contain `fromPromiseSettledResult` but almostly well done.

--- packages/option-t/src/plain_result/internal/intermediate_factory.ts
+++
@@ -1,9 +0,0 @@
-export { fromPromiseSettledResultToResult as fromPromiseSettledResult } from '../operators/from_promise_settled_result.js';
-export {
-    tryCatchIntoResult as tryCatchInto,
-    tryCatchIntoResultWithEnsureError as tryCatchIntoWithEnsureError,
-} from '../operators/try_catch.js';
-export {
-    tryCatchIntoResultAsync as tryCatchIntoAsync,
-    tryCatchIntoResultWithEnsureErrorAsync as tryCatchIntoWithEnsureErrorAsync,
-} from '../operators/try_catch_async.js';
